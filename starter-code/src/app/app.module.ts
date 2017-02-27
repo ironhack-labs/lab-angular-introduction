1b1904de6efa58b993f2cf55c8070dc16fcdfb55
ahuertam <ahuertam@yahoo.es>
2017-02-27 22:41:39 +0100

    done without css

---
+++ starter-code/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ChampionshipComponent } from './championship/championship.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    CommentsComponent,
+    ChampionshipComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ starter-code/src/app/comments/comments.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comments',
+  template: `
+    <p>
+      comments Works!
+    </p>
+    <p>
+      comments Works!
+    </p>
+    <p>
+      comments Works!
+    </p>
+    <p>
+      comments Works!
+    </p>
+    <p>
+      comments Works!
+    </p>
+    <p>
+      comments Works!
+    </p>
+    <p>
+      comments Works!
+    </p>
+    <p>
+      comments Works!
+    </p>
+  `,
+  styles: []
+})
+export class CommentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
