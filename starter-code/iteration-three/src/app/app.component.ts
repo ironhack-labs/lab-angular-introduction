185649627f9a9e1e464da82f34d7457cbb12db20
Anton Virtanen <avirtanen@hotmail.fr>
2017-02-27 19:44:14 +0100

    done

--- starter-code/iteration-three/src/app/app.component.ts
+++ starter-code/iteration-three/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  championships: Array<any>=[{
+    year: '2016',
+    name: 'Cleveland Cavaliers',
+    url:'http://logos-download.com/wp-content/uploads/2016/04/Cleveland_Cavaliers_logo.jpg',
+    best_players: ['Lebron James', 'Stephen Curry'],
+  },{
+    year: '2015',
+    name: 'Golden State Warriors',
+    url: 'http://i.cdn.turner.com/nba/nba/teamsites/images/legacy/warriors/LogoPrimary_300x329.jpg',
+    best_players: ['Stephen Curry', 'Lebron James'],
+  }];
 }
