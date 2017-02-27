c8313734c8f912f83689f253e1b01d0e22c5863c
Anton Virtanen <avirtanen@hotmail.fr>
2017-02-27 19:15:49 +0100

    iteration-two done

--- starter-code/iteration-two/src/app/app.component.ts
+++ starter-code/iteration-two/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+       <div>
+           <h1> {{title}} </h1>
+           <img src={{url}}>
+           <p>{{description}}</p>
+           <hr>
+           <app-comments></app-comments>
+        <div>
+`
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string = "Trump shrank";
+  url: string = "http://i.dailymail.co.uk/i/pix/2017/02/18/00/3D5BC94800000578-4236568-image-a-35_1487377656002.jpg";
+  description: string ="The nation is chocked as Trump has shrank. Like his ego."
 }
--- starter-code/iteration-two/src/app/app.module.ts
+++ starter-code/iteration-two/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
