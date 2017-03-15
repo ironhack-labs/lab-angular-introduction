fdb6b7fbe7a899927951f8e7b9816f04d523cb9d
IronHKristy <kristynhorton85@gmail.com>
2017-03-15 11:05:02 -0400

    angular

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//for local file imports, add the . first
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
