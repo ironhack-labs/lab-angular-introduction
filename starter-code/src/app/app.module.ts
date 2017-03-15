ebd7008516b712b2f0b0e3cfd98fd154c132d2ac
Walter Monecke <wmonecke@gmail.com>
2017-03-15 11:57:21 -0400

    Done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MainComponentComponent } from './main-component/main-component.component';
+import { CommentComponentComponent } from './main-component/comment-component/comment-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponentComponent,
+    CommentComponentComponent,
   ],
   imports: [
     BrowserModule,
---
+++ starter-code/src/app/main-component/comment-component/comment-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-component',
+  templateUrl: './comment-component.component.html',
+  styleUrls: ['./comment-component.component.css']
+})
+export class CommentComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
