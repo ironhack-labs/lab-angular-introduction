fd8f3806a69e19104b6bbb93c9aa17ca17fd81e1
Doyen Brown <doyen.brown@gmail.com>
2017-03-15 09:11:52 -0400

    2nd Iteration

--- angular-Iteration3/src/app/app.component.ts
+++ angular-Iteration3/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'IronHack Photo Album';
-
+  title = 'app works!';
 }
---
+++ angular-Iteration3/src/app/champion/champion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-champion',
+  templateUrl: './champion.component.html',
+  styleUrls: ['./champion.component.css']
+})
+export class ChampionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
