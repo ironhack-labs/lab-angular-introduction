514d74791a456607d2edd29c8c9248d89e39d33b
Adel Acebal <adelacebal@yahoo.com>
2017-06-13 22:18:18 -0400

    first iteration done

--- starter-code/src/app/app.component.ts
+++ starter-code/src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  photoAlbum: Object[] = [
+  {
+    src: '../assets/IMG_1.jpg',
+    description: 'NBV Bridge'
+  },
+  {
+    src: '../assets/IMG_2.jpg',
+    description: 'NBV Boat'
+  },
+  {
+    src: '../assets/IMG_3.jpg',
+    description: 'NBV Tree'
+  }];
 }
