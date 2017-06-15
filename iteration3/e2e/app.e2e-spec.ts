05ae17921223612b8a8f2a159bc6da65a518e54c
Alejandro <ajimenez1724@gmail.com>
2017-06-15 17:41:39 -0400

    done

---
+++ iteration3/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Iteration3Page } from './app.po';
+
+describe('iteration3 App', () => {
+  let page: Iteration3Page;
+
+  beforeEach(() => {
+    page = new Iteration3Page();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
