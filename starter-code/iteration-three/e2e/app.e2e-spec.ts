b46302df7dd79dd5c0daef91959858e1f47a7042
ogm710811 <ogm710811@gmail.com>
2017-06-16 00:05:04 -0400

    iteration-1 done

---
+++ starter-code/iteration-three/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { IterationThreePage } from './app.po';
+
+describe('iteration-three App', () => {
+  let page: IterationThreePage;
+
+  beforeEach(() => {
+    page = new IterationThreePage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
