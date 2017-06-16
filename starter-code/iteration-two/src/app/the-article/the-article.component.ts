66b2ab02eacf849719f7549dc57db94219f09c58
ogm710811 <ogm710811@gmail.com>
2017-06-16 01:00:01 -0400

    iteration-2 done

---
+++ starter-code/iteration-two/src/app/the-article/the-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-the-article',
+  templateUrl: './the-article.component.html',
+  styleUrls: ['./the-article.component.css']
+})
+export class TheArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ starter-code/iteration-two/src/app/the-comments/the-comments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-the-comments',
+  templateUrl: './the-comments.component.html',
+  styleUrls: ['./the-comments.component.css']
+})
+export class TheCommentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
