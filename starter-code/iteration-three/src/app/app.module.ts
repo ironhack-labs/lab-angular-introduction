4cd2c897170d3ac48852d3372b4cd3375828cb4b
ogm710811 <ogm710811@gmail.com>
2017-06-17 09:28:32 -0400

    iteration-3 done

--- starter-code/iteration-three/src/app/app.module.ts
+++ starter-code/iteration-three/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChampionshipsComponent } from './championships/championships.component';
+import { BestPlayersComponent } from './best-players/best-players.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChampionshipsComponent,
+    BestPlayersComponent
   ],
   imports: [
     BrowserModule
--- starter-code/iteration-two/src/app/the-article/the-article.component.ts
+++ starter-code/iteration-two/src/app/the-article/the-article.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Article {
+  title: string;
+  imageUrl: string;
+  description: string;
+}
+
+const DESCRIPTION = 
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et purus imperdiet,' +  
+    'tincidunt augue quis, eleifend justo. Vestibulum ac neque bibendum, aliquam eros sed,' + 
+    'faucibus lectus. Aenean pretium finibus tortor, ac ultricies ante mollis sit amet.' +
+    'Fusce sit amet vestibulum augue. Nunc in nulla eu magna ultricies eleifend.' +
+    'Pellentesque quis scelerisque augue, ut sagittis velit. Nam bibendum feugiat posuere.' +
+    'Mauris consectetur, lorem pretium sagittis consectetur, leo dolor egestas ligula,' +
+    'vel aliquet odio magna vel augue. Nunc ultricies nec erat eu facilisis. Sed gravida scelerisque dignissim.';
 @Component({
   selector: 'app-the-article',
   templateUrl: './the-article.component.html',
@@ -11,5 +25,11 @@ export class TheArticleComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  
+  
+  article : Article = {
+    title: 'Rounded Corners',
+    imageUrl: 'https://www.w3schools.com/bootstrap/cinqueterre.jpg',
+    description: DESCRIPTION,
+  }
 }
