20102def9ca167d6461e52bb5e7a5597eb390b43
ernestques <ernesqr@yahoo.com>
2017-06-19 19:44:12 -0400

    first iteration

--- starter-code/src/app/app.component.ts
+++ starter-code/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title:string = 'app works!';
+  photos: Object[]=[
+    { description:"running",
+      imageUrl:"https://static.pexels.com/photos/421160/pexels-photo-421160.jpeg"},
+      { description:"solar",
+      imageUrl:"https://static.pexels.com/photos/356049/pexels-photo-356049.jpeg"},
+      { description:"alone",
+      imageUrl:"https://static.pexels.com/photos/89854/pexels-photo-89854.jpeg"}
+      ]
 }
