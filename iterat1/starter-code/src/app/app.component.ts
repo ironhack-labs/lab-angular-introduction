c1f5fd1bb5c16afce15dfb3e0bdad8fbde8a790e
ernestques <ernesqr@yahoo.com>
2017-06-20 15:37:24 -0400

    it1

---
+++ iterat1/starter-code/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Photo album';
+  photos: Object[]=[
+    { description:"running",
+      imageUrl:"https://static.pexels.com/photos/421160/pexels-photo-421160.jpeg"},
+      { description:"solar",
+      imageUrl:"https://static.pexels.com/photos/356049/pexels-photo-356049.jpeg"},
+      { description:"alone",
+      imageUrl:"https://static.pexels.com/photos/89854/pexels-photo-89854.jpeg"}
+      ]
+}
