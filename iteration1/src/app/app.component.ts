3e62dcdb9429756f44bff5f71855e184e5036d5c
Dianelis Insua <dianelisinsua@gmail.com>
2017-06-28 13:32:54 -0400

    done with iteration1

--- iteration1/src/app/app.component.ts
+++ iteration1/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  image_url_1 = "https://www.ironhack.com/assets/images/courses/frontend-course-1.jpg";
+  image_title_1 = "Ironhack Madrid";
+  image_url_2 = "https://www.ironhack.com/assets/images/courses/frontend-course-2.jpg";
+  image_title_2 = "Ironhack Barcelona";
+  image_url_3 = "https://www.ironhack.com/assets/images/courses/frontend-course-3.jpg";
+  image_title_3 = "Ironhack Miami";
 }
