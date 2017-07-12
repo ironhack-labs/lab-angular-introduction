ff57aa93e425b3c5aefedb627b370196ba75fff3
davalarcon <zilentstorm@gmail.com>
2017-07-12 11:31:30 -0400

    70% done

--- starter-code/src/app/comments/comments.component.ts
+++ starter-code/src/app/comments/comments.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+// const Component = require ('@angular.core').Component;
+// const OnInit = require ('@angular.core').OnInit;
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css']
 })
+
+// module.exports = class FooterComponent
 export class CommentsComponent implements OnInit {
 
-  message: string = 'This are my comments'
+  // message: string = 'This are my comments'
 
   myComments: Object[]=[
     {name: 'Bobby Tables',
      comment: 'Angular looks like a very cool tool!! '
-    }
+   },
+   {name: 'David Alarcon',
+     comment:'  Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur.'}
   ];
 
   constructor() { }
