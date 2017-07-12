7c205b77167c4f1f91f74fe60d90138c679f8972
Nik E <NikolasEchenique@gmail.com>
2017-07-12 11:03:53 -0400

    not done, but they wantit to be submitted

--- starter-code/iteration-one/e2e/app.po.ts
+++ starter-code/iteration-one/e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class LabAngularIntroductionPage {
+export class IterationOnePage {
   navigateTo() {
     return browser.get('/');
   }
