f3cb0917534da8aefbd311202b0a98a44fb96e62
valzie <skyla@users.noreply.github.com>
2017-07-11 22:22:48 -0400

    tues night

---
+++ starter-code/src/app/photo3/photo3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo3',
+  templateUrl: './photo3.component.html',
+  styleUrls: ['./photo3.component.css']
+})
+export class Photo3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
