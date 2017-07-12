d53d734cb15b7d53c0b2494855c4f4da5c09d4d5
Jarrod Luca <jarrodluca@gmail.com>
2017-07-11 20:43:04 -0400

    undo stash, add components

---
+++ starter-code/src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'blog',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
