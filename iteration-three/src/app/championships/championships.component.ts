bc27b428876053883b3b575737c2127b26a0cd52
Jessicavandermarel <vandermareljessica@gmail.com>
2017-07-12 20:01:15 -0400

    done

--- iteration-three/src/app/championships/championships.component.ts
+++ iteration-three/src/app/championships/championships.component.ts
@@ -6,21 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./championships.component.css']
 })
 export class ChampionshipsComponent implements OnInit {
+ teams: Object[] =[
+   {
+      year: 2016,
+      name: "Cleveland Cavaliers",
+      championUrl: "https://www.pixelstalk.net/wp-content/uploads/2016/06/Cleveland-Cavaliers-Logo-Wallpaper-Download.jpg",
+      best_players: ["Lebron James", "Kyrie Irving", "Kevin Love", "J.R Smith"]
+    },
+    {
+      year: 2015,
+      name: "Golden State Warriors",
+      championUrl: "http://theawesomedaily.com/wp-content/uploads/2016/06/facts-about-the-golden-state-warriors-feat.jpg",
+      best_players: ["Kevin Durant", "Stephan Curry", "Klay Thompson", "Draymond Green"]
+    },
+    {
+      year: 2014,
+      name: "San Antonio Spurs",
+      championUrl: "https://pbs.twimg.com/profile_images/870330221370556416/5i_8BAc-_400x400.jpg",
+      best_players: ["Tim Duncan", "Tony Parker", "Manu Ginobili", "Tiago splitter"]
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-}
-
-let championslist = ["2105-16", "Cleveland Cavs"];
-
-class Champions {
-  constructor (
-    public year: number,
-    public teamname: string,
-    public url: string,
-    public bestPlayers: string,
-  ) {}
 }
