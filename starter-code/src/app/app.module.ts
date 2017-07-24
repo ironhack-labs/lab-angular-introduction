3ab47b21c1fe57dbeb8b1ac1b43a1f63e51febd9
Victor Rodriguez <victor.irix@gmail.com>
2017-07-24 18:01:18 +0200

    Iteration 1

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { IterationComponent } from './iteration/iteration.component';
+import { Iteration1Component } from './iteration-1/iteration-1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IterationComponent,
+    Iteration1Component
   ],
   imports: [
     BrowserModule,
---
+++ starter-code/src/app/iteration-1/iteration-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iteration-1',
+  templateUrl: './iteration-1.component.html',
+  styleUrls: ['./iteration-1.component.css']
+})
+export class Iteration1Component {
+  txtPic1 = 'Ferrodris Rules!';
+  txtPic2 = 'Ferrodris Rules!';
+  txtPic3 = 'Ferrodris Rules!';
+}
