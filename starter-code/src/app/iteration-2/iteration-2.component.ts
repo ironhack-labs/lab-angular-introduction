bf5d60cff7f065eae6a2a246709689a55d22d7fb
Daniele <daniele.germani.85@gmail.com>
2017-07-24 21:02:10 +0200

    done

---
+++ starter-code/src/app/iteration-2/iteration-2.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iteration-2',
+  templateUrl: './iteration-2.component.html',
+  styleUrls: ['./iteration-2.component.css']
+})
+export class Iteration2Component  {
+
+  title = "Reyes con su amigo";
+  url1 = "assets/back1.jpg";
+  descripcion = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+}
