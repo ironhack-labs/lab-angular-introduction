2f9777563bbc1fc1b6612c178d2c8408685e68fc
Juan Macias <yo@soyjuanmacias.com>
2017-07-24 20:36:52 +0200

    Iteration 1 and 2 ready

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ImagesComponent } from './images/images.component';
+import { Iteration2Component } from './iteration2/iteration2.component';
+import { Iteration3Component } from './iteration3/iteration3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImagesComponent,
+    Iteration2Component,
+    Iteration3Component
   ],
   imports: [
     BrowserModule,
