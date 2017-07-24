91c779dc1772a924a368fe62d5a196dca3c46a95
Marc Palatsí <mpalatsi@me.com>
2017-07-24 18:01:02 +0200

    iteration 1 done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Iteration1Component } from './iteration1/iteration1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Iteration1Component
   ],
   imports: [
     BrowserModule,
