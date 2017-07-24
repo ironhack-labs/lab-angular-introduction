6529bc59016215048f551a9c9f26038ff3576faa
mafevito <mafevito@hotmail.com>
2017-07-24 18:09:08 +0200

    Done iteration1

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Iteration1Component } from './iteration-1/iteration-1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Iteration1Component
   ],
   imports: [
     BrowserModule,
---
+++ starter-code/src/app/iteration-1/iteration-1.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iteration-1',
+  templateUrl: './iteration-1.component.html',
+  styleUrls: ['./iteration-1.component.css']
+})
+export class Iteration1Component {
+  text1 = "description1"
+  text2 = "description2"
+  text2 = "description3"
+}
