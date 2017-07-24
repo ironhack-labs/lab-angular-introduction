1a0f595b9c29821314dc8c2e100a089b44b37f04
Fernando Palma <ferpalma21@github.com>
2017-07-25 01:45:44 +0200

    iteration one just started

--- starter-code/src/app/app.component.ts
+++ starter-code/src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+export class Photo {
+    photoUrl: string;
+    name: string;
+}
+
+const pictures : Photo[] = [{
+  photoUrl : "http://gph.is/1swzxHT",
+  name : "One shot More!"
+},{
+  photoUrl : "http://gph.is/2lhtbi4",
+  name : "What you want?"
+},{
+  photoUrl : "http://gph.is/2cJDz1D",
+  name : "Welcome!"
+}
+]
+
 export class AppComponent {
   title = 'app works!';
 }
