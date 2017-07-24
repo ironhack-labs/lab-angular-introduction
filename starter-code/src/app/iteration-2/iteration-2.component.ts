7ea1cecc4b47e29128633ad7d2448040fb6b5d27
Victor Rodriguez <victor.irix@gmail.com>
2017-07-24 19:35:45 +0200

    DONE - Ugly

---
+++ starter-code/src/app/iteration-2/iteration-2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iteration-2',
+  templateUrl: './iteration-2.component.html',
+  styleUrls: ['./iteration-2.component.css']
+})
+export class Iteration2Component {
+title = "El meme de la Reyes";
+
+}
