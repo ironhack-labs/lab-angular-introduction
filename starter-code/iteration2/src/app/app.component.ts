689eecce93ad6b85b83886c074f34ee448ab5e77
wba0 <william.bonneyache@gmail.com>
2017-09-25 17:44:01 -0400

    on iteration 2

---
+++ starter-code/iteration2/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Learn how to code with Irohack';
+  mainImg: string = "https://images.unsplash.com/photo-1504964148034-86ded740d1e2?dpr=1&auto=compress,format&fit=crop&w=1950&h=&q=80&cs=tinysrgb&crop=";
+  introP: string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis repudiandae non magni, vitae repellendus cupiditate hic nulla delectus, sunt recusandae eius, voluptatibus ab ex tenetur ullam totam architecto unde? Modi.";
+
+}
