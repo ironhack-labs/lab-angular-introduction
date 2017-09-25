715676b2bede267b89c082550b9fde1fa0e7c474
philipwisner <philipwisner@gmail.com>
2017-09-25 21:24:31 +0200

    iteration 2 almost done

--- starter-code/Iteration-2/src/app/comments/comments.component.ts
+++ starter-code/Iteration-2/src/app/comments/comments.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent implements OnInit {
+  comments = [
+    { 
+     name: "Bobby",
+     comment: "It seems quite complicated, how much previous experience do I need?"
+    },
+    {
+    name: "IronHack",
+    comment: "We give you pre-work, don't worry!"
+    }
+  ]
 
   constructor() { }
 
--- starter-code/Iteration-2/src/app/main/main.component.ts
+++ starter-code/Iteration-2/src/app/main/main.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  title = "Learn how to code with Angular";
+  image = "/assets/pexels-photo-585418.jpg";
+  caption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare viverra consequat. Sed eu tortor dolor. Nulla facilisis lorem eu turpis condimentum, eget bibendum ex aliquam. Aliquam id augue sollicitudin, cursus purus sit amet, aliquam nisi. Proin consequat quis diam id feugiat. Nullam id risus eu nunc congue laoreet. Nam pellentesque purus eu velit posuere maximus. Praesent interdum risus vitae nulla lacinia lacinia et quis mauris. In non diam odio. Sed id ante eros. Quisque porttitor turpis in dui suscipit laoreet. Pellentesque pulvinar leo ut massa laoreet tincidunt. Nulla facilisi."
+
   constructor() { }
 
   ngOnInit() {
