65e3889ebdadc3a16bc82ba83e7133bdfb45294d
trigoporres <santiagotrigoporres@gmail.com>
2017-09-25 23:50:52 +0200

    primera iteracion

--- starter-code/src/app/app.component.ts
+++ starter-code/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  width = 20;
+
+  src = "http://mobileworldcapital.com/system/contents/images/article_595_contents_6763_capital_big.jpg";
+  description = "IRONHACK"
+
+  src1 = "http://blog.ironhack.com/wp-content/uploads/2017/05/full-stack-web-development-bootcamp-madrid-2-920x469.jpg";
+  description1 = "Alumnos Iron hack"
+
+  src2 = "http://blog.ironhack.com/wp-content/uploads/2016/07/hectorges-27-2-1024x683.jpg";
+  description2 = "Programando Iron hack"
+
+
 }
