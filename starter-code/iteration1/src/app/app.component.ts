901d79c65d59557512df632dd690eb1446df7d57
wba0 <william.bonneyache@gmail.com>
2017-09-25 16:47:41 -0400

    iteration 1 done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  photoList: any = [
+    {caption: "kitty photo", url: "https://media.giphy.com/media/VxbvpfaTTo3le/giphy.gif"},
+    {caption: "cat photo", url: "https://media.giphy.com/media/yFQ0ywscgobJK/giphy.gif"},
+    {caption: "lion photo", url: "https://media.giphy.com/media/3o6wrrNFa9OH9FFRpS/giphy.gif"}
+  ]
 }
