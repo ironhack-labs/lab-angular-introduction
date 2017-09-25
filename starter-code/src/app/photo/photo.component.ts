c7c6431576b4ac2991a0f03db4f63a0ac22aaa85
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-25 21:09:57 +0200

    Iteration #1: Photo Album done

--- starter-code/src/app/photo/photo.component.ts
+++ starter-code/src/app/photo/photo.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+
+interface photoObj{
+  imgSrc:string,
+  imgTxt:string
+}
 
 @Component({
   selector: 'app-photo',
@@ -6,6 +11,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo.component.css']
 })
 export class PhotoComponent implements OnInit {
+  photos: Array<photoObj> = [
+    {imgSrc: 'http://blog.ironhack.com/wp-content/uploads/2017/01/best-coding-botcamp-ironhack-student-podcast-blog-header.jpg', imgTxt: 'Primera foto'},
+    {imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuLV_uZSLy5sfV3VYc6vOOuXbohZoz0aijSEJq0gEAELd76G_d', imgTxt: 'Segunda foto'},
+    {imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShw986smDXGn-XIVSqTdMIPGtGyzPv7_imWlwa29j0LKLCl-RMMw', imgTxt: 'Tercera foto'}]
 
   constructor() { }
 
