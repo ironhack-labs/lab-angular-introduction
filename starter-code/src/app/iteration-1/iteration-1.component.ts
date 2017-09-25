b1e48714bc4aa0a98f201e5aec4601facce4c20f
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-09-25 21:45:35 +0200

    iteration 1 done

---
+++ starter-code/src/app/iteration-1/iteration-1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iteration-1',
+  templateUrl: './iteration-1.component.html',
+  styleUrls: ['./iteration-1.component.css']
+})
+export class Iteration1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
