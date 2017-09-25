fbe7a119ce3497310ce38c759f1dc833e2632ac0
veroglez <veronica.glezperez@gmail.com>
2017-09-25 19:26:59 +0200

    Iteration 1 done

--- starter-code/src/app/app.component.ts
+++ starter-code/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  img1 = 'https://www.arquitecturayempresa.es/sites/default/files/content/arquitecturayempresa_ironhack_02.jpg';
+  img2 = 'https://okdiario.com/img/2016/05/iron-hack.jpg';
+  img3 = 'https://educartis-production.s3.amazonaws.com/images/original-c7af33ea172283b1462804ba14e47b4262abb1c6.jpg?1464248758';
+  width = 200;
+  text1 = 'Ironhack Madrid';
+  text2 = 'Ironhack Barcelona';
+  text3 = 'Ironhack Miami';
 }
