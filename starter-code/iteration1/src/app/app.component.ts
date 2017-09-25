ea4c1f4b2f488bbba1a44c5f2a77eb4ae5f56e5a
luisa86 <lfsh86@gmail.com>
2017-09-25 22:49:02 +0200

    empezando iteracion 1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  image1 = 'http://delpozoaluniverso.com/wp-content/uploads/2017/07/ironhack-6-semana-segundo-proyecto-585x390.jpeg';
+  image2 = 'https://www.ironhack.com/assets/images/courses/frontend-course-1.jpg';
+  image3 = 'https://www.ironhack.com/assets/images/courses/front-end-course-3.jpg';
+  title1 = 'Ironhack Madrid';
+  title2 = 'Ironhack Barcelona';
+  title3 = 'Ironhack Miami';
 }
--- starter-code/iteration1/src/app/app.module.ts
+++ starter-code/iteration1/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
