d79b2aeb5e07ba0bd8dde9a81d45f963c282ec07
philipwisner <philipwisner@gmail.com>
2017-09-26 09:21:02 +0200

    updated iteration 1

--- starter-code/Iteration-1/src/app/photo-album/photo-album.component.ts
+++ starter-code/Iteration-1/src/app/photo-album/photo-album.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoAlbumComponent implements OnInit {
   title = 'Photos';
+
+  pictures = [
+  {
+  src: "/assets/pexels-photo.jpg",
+  caption: "Construction Worker 1"
+  },
+  {
+  src: "/assets/pexels-photo-585418.jpg",
+  caption: "Construction Worker 2"
+  },
+  {
+  src: "/assets/pexels-photo-585419.jpg",
+  caption: "Construction Worker 3"
+  }
+];
+
   constructor() { }
 
   ngOnInit() {
