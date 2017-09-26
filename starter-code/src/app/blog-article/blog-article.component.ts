2e458fe6e0b4263496bb85210295304d54025d8b
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-26 09:17:20 +0200

    Muestra el articulo del blog

--- starter-code/src/app/blog-article/blog-article.component.ts
+++ starter-code/src/app/blog-article/blog-article.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-article.component.css']
 })
 export class BlogArticleComponent implements OnInit {
+  title:string = 'Learn how to code with IronHack'
+  imgSrc:string = 'https://cdn.thinglink.me/api/image/661234398170972161/1240/10/scaletowidth'
+  imgAlt:string = 'Madrid'
+  description:string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum felis ut nunc pulvinar, eu facilisis lectus molestie. Nunc vel consectetur eros, non condimentum risus. Maecenas ornare id odio sit amet posuere. Etiam dignissim cursus ultrices. Nunc fringilla, justo ac tincidunt feugiat, nisi odio elementum libero, a scelerisque odio magna eget nunc. Nam ornare iaculis eros. Vestibulum justo erat, luctus at libero in, feugiat lobortis nisi. Morbi at lacinia orci. Suspendisse commodo nisi vitae finibus vehicula.'
 
   constructor() { }
 
