8eeae8567e22d48cc4c201ba43bc6dbc560b8490
luisa86 <lfsh86@gmail.com>
2017-09-26 09:09:23 +0200

    hecha iteracion1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  image1 = 'http://delpozoaluniverso.com/wp-content/uploads/2017/07/ironhack-6-semana-segundo-proyecto-585x390.jpeg';
-  image2 = 'https://www.ironhack.com/assets/images/courses/frontend-course-1.jpg';
-  image3 = 'https://www.ironhack.com/assets/images/courses/front-end-course-3.jpg';
-  title1 = 'Ironhack Madrid';
-  title2 = 'Ironhack Barcelona';
-  title3 = 'Ironhack Miami';
+  
 }
--- starter-code/iteration1/src/app/image/image.component.ts
+++ starter-code/iteration1/src/app/image/image.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image.component.css']
 })
 export class ImageComponent implements OnInit {
-
+  image1 = 'http://delpozoaluniverso.com/wp-content/uploads/2017/07/ironhack-6-semana-segundo-proyecto-585x390.jpeg';
+  image2 = 'https://www.ironhack.com/assets/images/courses/frontend-course-1.jpg';
+  image3 = 'https://www.ironhack.com/assets/images/courses/front-end-course-3.jpg';
+  title1 = 'Ironhack Madrid';
+  title2 = 'Ironhack Barcelona';
+  title3 = 'Ironhack Miami';
   constructor() { }
 
   ngOnInit() {
