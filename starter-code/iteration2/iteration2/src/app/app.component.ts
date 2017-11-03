ab02d91a9fd3efa4d566ed95d01e9e7ba525193a
Manuel <m.a.c.p90@gmail.com>
2017-11-03 16:52:12 -0400

    adding pictures of first iteration

---
+++ starter-code/iteration2/iteration2/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  picture = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPZvhDSoP1B-Ox9jirsRGWoek5-83E57_CnSdQnV64VZDgoyHw"]
+}
