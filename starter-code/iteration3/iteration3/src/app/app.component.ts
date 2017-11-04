fa230339ebcf15f00a59179e0fbee43d840537f2
Manuel <m.a.c.p90@gmail.com>
2017-11-04 08:22:01 -0400

    comment sction nested

--- starter-code/iteration3/iteration3/src/app/app.component.ts
+++ starter-code/iteration3/iteration3/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'learn how to code in ironhack';
+  content = 'here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.'
+ image= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa7pgDmY_Kz3vLJQ67E8BdGwKqeypI8A7noO-4-zmesN6Z4ERH'
 }
--- starter-code/iteration3/iteration3/src/app/app.module.ts
+++ starter-code/iteration3/iteration3/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyNestedComponent } from './my-nested/my-nested.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNestedComponent,
+  
   ],
   imports: [
     BrowserModule
