5a4ce363db55a5339b5bd78f9ab246fe3c0f458a
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-05 19:50:39 -0500

    Finished iteration 1

--- starter-code/iteration1/src/app/app.module.ts
+++ starter-code/iteration1/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
