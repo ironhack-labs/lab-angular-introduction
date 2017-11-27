8bff297bf08a88e0d8fa16cb7366c108abfa2539
popelini <ccastillon92@gmail.com>
2017-11-27 18:40:49 +0100

    Iteration 1 finished

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Photo Album!';
+  content = [
+    {
+      url: 'http://s.newsweek.com/sites/www.newsweek.com/files/2017/10/09/10-9-rick-morty-mcdonalds.jpg',
+      name: 'Rick Sanchez: Wubalubadubdub!!',
+    }, {
+      url: 'https://cdn.pastemagazine.com/www/articles/morty%20main.jpg',
+      name: 'Morty Smith: I shat my pants!'
+    }, {
+      url: 'https://nerdist.com/wp-content/uploads/2016/09/Mr-Meeseeks.jpg',
+      name: 'Mr Meeseeks I/\'/m Mr. Meeseeks! Look at me!'
+    }
+  ];
 }
