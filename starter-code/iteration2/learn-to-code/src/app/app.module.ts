c01d52a0a2dcb93644909358ca8c45234c322846
Sara <saraeme20@gmail.com>
2017-11-27 19:34:42 +0100

    iterations 1 and 2

---
+++ starter-code/iteration2/learn-to-code/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MyNestedComponent } from './my-nested/my-nested.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyNestedComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
