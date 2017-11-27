e96566a26a5172fc2060e3a9322f325fda679afc
3lv27 <elvin.gm27@gmail.com>
2017-11-27 20:36:09 +0100

    iteration 2

---
+++ starter-code/iteration2/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: String = 'Lear how to code with IronHack';
+  image: String = 'https://img.buzzfeed.com/buzzfeed-static/static/2014-06/2/8/enhanced/webdr06/original-31994-1401713137-15.jpg?downsize=715:*&output-format=auto&output-quality=auto';
+  description: String = 'Holaaaaaa';
+}
