1e55e3ea45fff3202a98f95e9371883b4aad9528
Torgeir Beyer <torgeir.beyer@gmail.com>
2017-11-27 22:06:32 +0100

    it 2 done

--- starter-code/iteration2/src/app/comments/comments.component.ts
+++ starter-code/iteration2/src/app/comments/comments.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent implements OnInit {
-  author: string = `Torgeir`;
-  text: string = `Bacon ipsum dolor amet kielbasa sirloin strip steak pork turducken rump ground round. Biltong turkey bresaola, andouille meatloaf tri-tip leberkas short loin rump shankle swine tongue. Sausage jowl strip steak, brisket biltong spare ribs shankle pork belly pig ham ham hock turducken salami filet mignon.`
+  author = `Torgeir`;
+  text = `Bacon ipsum dolor amet kielbasa 
+  sirloin strip steak pork turducken rump ground round. 
+  Biltong turkey bresaola, andouille meatloaf tri-tip 
+  leberkas short loin rump shankle swine tongue. 
+  Sausage jowl strip steak, brisket biltong spare ribs 
+  shankle pork belly pig ham ham hock turducken 
+  salami filet mignon.`;
 
   constructor() { }
 
