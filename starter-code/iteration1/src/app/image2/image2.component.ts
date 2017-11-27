807ad634c5799c16dd51472eaab7c34034c10a39
J. Gaspar Prieto <jgprieto00@gmail.com>
2017-11-27 19:20:19 +0100

    second iteration

---
+++ starter-code/iteration1/src/app/image2/image2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image2',
+  templateUrl: './image2.component.html',
+  styleUrls: ['./image2.component.css']
+})
+export class Image2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ starter-code/iteration1/src/app/image3/image3.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Image3Component } from './image3.component';
+
+describe('Image3Component', () => {
+  let component: Image3Component;
+  let fixture: ComponentFixture<Image3Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Image3Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Image3Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
