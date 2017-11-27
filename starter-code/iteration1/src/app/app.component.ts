39ae065b5709ecd8251b1f1f4701d0e02656c838
Sarah Fontes <sarahfontes@Sarahs-MacBook-Pro.local>
2017-11-27 23:30:57 +0100

    second iteration

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'album';
+  title: string = 'Album';
   album: { src: string, description: string}[] = [
     {
       src: '../assets/photo1.png',
