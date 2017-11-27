64d8a7034605b099764c2355e0e82281cd281e82
Camila Maya <camilamaya21@gmail.com>
2017-11-27 18:31:48 +0100

    First iteration Done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Gatetes! ^-^';
+  imgAlbum = [
+    {img : "https://i.pinimg.com/736x/35/8c/57/358c57c204a2fec21fa50b917a0728aa--rainbow-face-rainbow-prism.jpg",
+      description : "Rainbow Kitty"},
+    {img : "https://pbs.twimg.com/media/C5iQlTzVMAAu65S.jpg",
+      description : "Kitty :P"},
+    {img : "https://media.mnn.com/assets/images/2016/11/cat-with-big-eyes-gimo.jpg.653x0_q80_crop-smart.jpg",
+      description : "Sweet Eyes"},
+  ];
+  heightcat = "200";
 }
