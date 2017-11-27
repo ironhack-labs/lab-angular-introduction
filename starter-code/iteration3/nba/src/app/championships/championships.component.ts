628d1f10d31878a5a54e1039639a7b56011101c5
Nikolaas Verlee <nikotomad@gmail.com>
2017-11-27 20:06:30 +0100

    Iteration 3 done

---
+++ starter-code/iteration3/nba/src/app/championships/championships.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-championships',
+  templateUrl: './championships.component.html',
+  styleUrls: ['./championships.component.css']
+})
+export class ChampionshipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
