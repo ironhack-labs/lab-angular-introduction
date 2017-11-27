07f9d07242e8dd1c930f342501df5f3d8e80c0e7
Asier Cisneros <ascisneros14@gmail.com>
2017-11-27 20:46:23 +0100

    finished all iterations

---
+++ starter-code/iteration2/article/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Learn to code with Ironhack';
+  img = 'https://okdiario.com/img/2016/05/iron-hack.jpg'
+  text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ipsum nisi. Maecenas non posuere ipsum. Aliquam erat volutpat. Donec eu nisi magna. Morbi ac dapibus nisl. Sed non mauris ut ex posuere finibus. Curabitur volutpat tincidunt risus, sit amet placerat ipsum consequat non. Fusce velit elit, congue a nunc vitae, ornare ornare odio. Nullam rutrum ultrices velit vel facilisis. Curabitur vel posuere lacus. Sed sem risus, auctor id mi eget, malesuada porta orci. Duis pellentesque purus urna, quis bibendum nisl fringilla at. Sed sit amet laoreet sem.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non eros et risus finibus pretium a a tellus. Ut semper eros nec tellus hendrerit efficitur. Nunc volutpat blandit augue scelerisque sodales. Vivamus vehicula hendrerit dui, quis dignissim mi. Integer tempus consectetur diam, eu porta est sollicitudin ut. Aenean malesuada tincidunt metus, vel vulputate odio condimentum et.'
+}
