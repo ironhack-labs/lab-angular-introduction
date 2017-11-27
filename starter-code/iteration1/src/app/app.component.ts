e5d4c8e97ddca96ce95b0e7b069b17c7173ebd5a
Dafne <olca.dafne@gmail.com>
2017-11-27 21:36:27 +0100

    iteration 1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "app works!";
+  img1 = {
+    src:
+      "https://www.wired.com/images_blogs/rawfile/2013/10/DeathtoStock8-200x100.jpg",
+    comment: "larifari"
+  };
+  img2 = {
+    src:
+      "https://photos.the-scientist.com/legacyArticleImages/1993/04/Stock-market-200x100.jpg",
+    comment: "larifari"
+  };
+  img3 = {
+    src: "http://www.oceanstockimages.com.au/img/s/v-2/p764504336-11.jpg",
+    comment: "larifari"
+  };
 }
---
+++ starter-code/iteration2/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MainComponentComponent } from './components/main-component/main-component.component';
+import { MainComponentsComponent } from './components/main-components/main-components.component';
+import { CommentsComponent } from './components/comments/comments.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    MainComponentComponent,
+    MainComponentsComponent,
+    CommentsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
