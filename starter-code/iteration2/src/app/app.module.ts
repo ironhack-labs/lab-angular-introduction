8ee71ad099fa4626d282875e4c8d6106eb313f94
popelini <ccastillon92@gmail.com>
2017-11-27 19:29:57 +0100

    Iteration 2 Finished

---
+++ starter-code/iteration2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MaincomponentComponent } from './maincomponent/maincomponent.component';
+import { CommentscomponentComponent } from './commentscomponent/commentscomponent.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MaincomponentComponent,
+    CommentscomponentComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ starter-code/iteration2/src/app/maincomponent/maincomponent.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maincomponent',
+  templateUrl: './maincomponent.component.html',
+  styleUrls: ['./maincomponent.component.css']
+})
+export class MaincomponentComponent implements OnInit {
+
+  title = 'Learn to code in Ironhack';
+  image = 'http://endesavehiculoelectrico.com/wp-content/uploads/2017/08/madrid.jpg';
+  description = 'Lorem fistrum al ataquerl va usté muy cargadoo apetecan te va a hasé pupitaa. Hasta luego Lucas sexuarl te voy a borrar el cerito se calle ustée mamaar. Jarl papaar papaar quietooor a wan va usté muy cargadoo. Llevame al sircoo te voy a borrar el cerito amatomaa diodeno sexuarl diodeno me cago en tus muelas. La caidita fistro a gramenawer hasta luego Lucas condemor mamaar. Tiene musho peligro ese que llega ese pedazo de no puedor. Ahorarr caballo blanco caballo negroorl se calle ustée jarl torpedo me cago en tus muelas mamaar apetecan llevame al sircoo. Qué dise usteer me cago en tus muelas apetecan de la pradera diodeno la caidita me cago en tus muelas te va a hasé pupitaa no te digo trigo por no llamarte Rodrigor.';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
