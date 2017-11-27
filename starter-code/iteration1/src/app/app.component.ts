3d53b148588ffca6b6cbe6d57c14002a0748f45b
FranRom <maildefranz@gmail.com>
2017-11-27 19:19:38 +0100

    First iteration

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app works!';
+  info:Array<any> = [
+    {
+      img: 'http://www.elreferente.es/source/ironhack-hackshow-interior.jpg',
+      desc:'Ironhack Madrid'
+
+    },
+    {
+      img: 'https://cdn.evbuc.com/eventlogos/130268051/img3301128129.jpg',
+      desc: 'Ironhack Miami'
+    },
+   {
+     img: 'http://blog.ironhack.com/wp-content/uploads/2016/07/hectorges-52-1024x683.jpg',
+     desc: 'Ironhack Barcelona'
+   }
+  ]
+
 }
