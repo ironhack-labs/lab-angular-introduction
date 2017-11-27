a2dbc365980dc7350d81b63fa167e72a23a41a49
3lv27 <elvin.gm27@gmail.com>
2017-11-27 19:40:55 +0100

    1 iteration

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  pictures: Array<Object> = [
+    {
+      src: 'https://okdiario.com/img/2016/05/iron-hack.jpg',
+      name: 'IronHack Barcelona'
+    }, {
+      src: 'http://www.elreferente.es/source/ironhack-hackshow-interior.jpg',
+      name: 'IronHack Madrid'
+    }, {
+      src: 'http://media.diariolasamericas.com/adjuntos/216/imagenes/000/105/0000105028.jpg',
+      name: 'IronHack Miami'
+    }
+  ];
 }
