e959abeec1649eb4f8e2cdb445d928fd6f55bc43
bryan <supercrew3@gmail.com>
2017-11-28 09:27:23 +0100

    iteration1,2

---
+++ starter-code/iteration2/angular-component/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class MainComponent {
+  title = 'Learn how to learn with Ironhack';
+  image = `https://cdn.evbuc.com/eventlogos/130268051/dsc028128129.jpg`;
+  description = `Today, most people don't need to know how a computer works.  Most people can simply turn on a computer or a mobile phone and point at some little graphical object on the display, click a button or swipe a finger or two, and the computer does something.  `;
+}
