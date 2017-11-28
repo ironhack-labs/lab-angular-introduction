e84264b27f53557336bb517e1adfa007d56ad70b
Sara Morales <saringa@gmail.com>
2017-11-28 09:12:05 +0100

    it1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "IH Locations!";
+
+  img1 = {
+    src: "assets/barcelona.jpg",
+    comment: "IH BCN"
+  };
+  img2 = {
+    src: "assets/madrid.jpg",
+    comment: "IH MAD"
+  };
+  img3 = {
+    src: "assets/miami.jpg",
+    comment: "IH MIA"
+  };
 }
