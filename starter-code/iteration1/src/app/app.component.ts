e45aa99b9c9c7b207444e7ea7fa63a4ee3dd6bbd
Alex Del Valle <amd407@gmail.com>
2017-11-30 18:24:33 -0500

    Iteration 1 code complete

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'photo-reel',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Our Adventures';
+
+  myPhotos: any[] = [
+      {
+        title: "Talking in Toronto",
+        imageUrl: "https://media.giphy.com/media/yPhqlJccIOaru/giphy.gif"
+      },
+      {
+        title: "Diva-ing in Denver",
+        imageUrl: "https://media.giphy.com/media/cMw0qTJjnFXeE/giphy.gif"
+      },
+      {
+        title: "Running in Rhode Island",
+        imageUrl: "https://media.giphy.com/media/7kn27lnYSAE9O/giphy.gif"
+      }
+  ]
+
+
 }
