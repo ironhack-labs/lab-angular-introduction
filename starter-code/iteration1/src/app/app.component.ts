b292de7473a045463c498d5816d15e824ed30211
Jessica Alin Gonzalez <jessicaalin@Jessicas-MacBook-Pro.local>
2017-11-30 18:25:39 -0500

    finished iteration 1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Kitty Photo Album';
+
+  myPhotos: any[] = [
+    {
+      imageUrl: "https://media.giphy.com/media/11s7Ke7jcNxCHS/giphy.gif",
+      description: "Super cute kitty stretching."
+    },
+    {
+      imageUrl: "https://media.giphy.com/media/LNisSS34qHwcg/giphy.gif",
+      description: "Super cute kitty meowing on a piggy pillow."
+    },
+    {
+      imageUrl: "https://media.giphy.com/media/K1wjOn6HImv7y/giphy.gif",
+      description: "Super cute kitty yawning."
+    }
+  ]
+
 }
