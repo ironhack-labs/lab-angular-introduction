73f1cdaad9c968c8894517ac9497d46e0075daeb
Jessica Alin Gonzalez <jessicaalin@Jessicas-MacBook-Pro.local>
2017-12-01 09:53:51 -0500

    finished iteration 2

---
+++ starter-code/iteration2/src/app/main/main.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+
+export class MainComponent implements OnInit {
+
+  image = 'https://media.giphy.com/media/VxbvpfaTTo3le/giphy.gif';
+  description = 'Scratch the box eat too much then proceed to regurgitate all over living room carpet while humans eat dinner pounce on unsuspecting person lick yarn hanging out of own butt so eat half my food and ask for more, i shredded your linens for you. Get video posted to internet for chasing red dot that box? i can fit in that box or warm up laptop with butt lick butt fart rainbows until owner yells pee in litter box hiss at cats meow meow for kitten is playing with dead mouse, but jump five feet high and sideways when a shadow moves open the door, let me out, let me out, let me-out, let me-aow, let meaow, meaow!';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
