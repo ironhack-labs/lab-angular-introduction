409f7686d0541d07dca13603d845c80a90eae9f0
Jesus Bordones <jabordones92@gmail.com>
2017-12-01 10:08:38 -0500

    iteration1 done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'World Cup Favorites!';
+
+  myAlbum: any[] =[
+    {
+      imageUrl: "https://media.giphy.com/media/3o6gEaYbewKku0GwPS/giphy.gif",
+      description: "France"
+    },
+    {
+      imageUrl: "https://media.giphy.com/media/ym4YttqYt6WwU/giphy.gif",
+      description: "Germany"
+    },
+    {
+      imageUrl: "https://media.giphy.com/media/u1zLnbcenLaxy/giphy.gif",
+      description: "Spain"
+    }
+  ]
 }
