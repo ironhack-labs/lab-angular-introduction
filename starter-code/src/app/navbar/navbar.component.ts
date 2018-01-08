954a081d48a709387b526782a152f357c7120b24
polgasull <pol89.gn@gmail.com>
2018-01-09 00:49:54 +0100

    new navbar and fluid footer

--- starter-code/src/app/navbar/navbar.component.ts
+++ starter-code/src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  title = 'Catalonia Places'
   constructor() { }
 
   ngOnInit() {
