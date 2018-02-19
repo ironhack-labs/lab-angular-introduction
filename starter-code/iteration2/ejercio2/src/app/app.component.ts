b3de3448eb0b7ad8332d0b84423e52ef1a4bcf5e
Sébastien Del Mestre <sebastien.delmestre@gmail.com>
2018-02-19 18:45:20 +0100

    second iteration done

---
+++ starter-code/iteration2/ejercio2/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title:string = 'Learn how to code with Ironhack';
+  image:string = 'http://delpozoaluniverso.com/wp-content/uploads/2017/07/Equipo-Ironhack.jpg';
+  description:string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est libero, tincidunt a auctor nec, tempus vel velit. Sed erat risus, pretium ut maximus ut, cursus sit amet velit. Curabitur eu lobortis dolor. Etiam eget lectus velit. Duis sagittis, est eget pellentesque gravida, nisl risus facilisis elit, vel tempor eros nunc vel lectus. Quisque convallis et est nec egestas. Maecenas laoreet ultrices nisi quis facilisis. Etiam cursus consequat vestibulum. Nullam placerat lacus erat, vel porta erat interdum vel. Curabitur eleifend a leo sit amet volutpat.';
+}
