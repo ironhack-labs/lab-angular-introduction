f62a4d5e8ad691454705d03169159a74d4adece0
Pere Amorós <amorospere@gmail.com>
2018-02-19 20:45:43 +0100

    Iteration 2 - finished

--- starter-code/iteration2/src/app/components/comments/comments.component.ts
+++ starter-code/iteration2/src/app/components/comments/comments.component.ts
@@ -6,16 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent implements OnInit {
-  comments: Array<string> = ['Aquest article és molt bo! te\'l recomano.', ''];
-  
+  comments: Array<string> = ['Aquest article és molt bo! te\'l recomano.'];
+
   // comments: Array<{}> = [{
   //   name: '',
   //   text: ''
   // }];
+  
   constructor() { }
 
-  addComment(comment){
-    this.comments.push(comment)
+  addComment(input){
+    this.comments.push(input.value.comment)
     console.log(this.comments);
   }
 
