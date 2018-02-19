4da6b19a93061b8b284ad9c96cc56c76aee1c236
Kathia Rieser <kath.medulla@gmail.com>
2018-02-19 17:16:28 +0100

    iteracion 1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  url_img = 'http://via.placeholder.com/350x150';
+  description = "Ironhack Madrid";
 }
