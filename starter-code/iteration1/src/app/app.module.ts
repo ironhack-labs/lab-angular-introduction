beb9cee14779fea1d852f0abbeb419df65dc1ab7
xarliex <carlos.lorente@hotmail.com>
2018-02-19 17:53:18 +0100

    iteration1 done

--- starter-code/iteration1/src/app/app.module.ts
+++ starter-code/iteration1/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyNestedComponent } from './my-nested/my-nested.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNestedComponent
   ],
   imports: [
     BrowserModule,
---
+++ starter-code/iteration1/src/app/my-nested/my-nested.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-nested',
+  templateUrl: './my-nested.component.html',
+  styleUrls: ['./my-nested.component.css']
+})
+export class MyNestedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
