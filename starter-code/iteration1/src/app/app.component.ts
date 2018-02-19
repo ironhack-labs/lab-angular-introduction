9680aefa0e32bac8610b3785fc535defe3dcdd8d
Ana Recuero <anarecsa@gmail.com>
2018-02-19 16:53:22 +0100

    iteration 1 done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title1:string = 'Dino!';
+  imgUrl1:string = "http://www.imgworlds.com/wp-content/themes/IMG/img/phase3/welcome/trex.png";
+  title2:string = 'Flamenco!';
+  imgUrl2:string = "https://html.com/wp-content/uploads/very-large-flamingo.jpg";
+  title3:string = 'Mariposa!';
+  imgUrl3:string = "https://s-media-cache-ak0.pinimg.com/originals/d4/7f/ca/d47fcad552ec92db168d2dfea77eb408.jpg";
 }
