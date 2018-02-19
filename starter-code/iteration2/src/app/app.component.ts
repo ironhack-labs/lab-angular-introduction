943b34e2678319b22cf33677972d71a72e475961
Jana Stinn <jana-stinn@gmx.de>
2018-02-19 20:33:49 +0100

    finished

---
+++ starter-code/iteration2/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Ironhack';
+  imgUrl = 'http://via.placeholder.com/350x150';
+  description = 'Ironhack BCN';
+}
+
---
+++ starter-code/iteration3/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  champions = [
+    {
+     year: '2015-2016',
+     team: 'Cleveland Cavaliers',
+     logo: 'http://bestdecalsonline.com/thumbnail.asp?file=assets/images/10500-CC01%20%20Cleveland%20Cavaliers%20(A).jpg&maxx=300&maxy=0',
+     players: ['Lebron James', 'Kyrie Irving', 'Kevin Love', 'J.R Smith', 'Anderson Varejao'];
+    },
+
+    {
+    year: '2014-2015',
+    team: 'Golden State Warriors',
+    logo: 'http://www.dineshopplay.com/wp-content/uploads/sites/www.dineshopplay.com/images/event/7735/warriors.jpg',
+    players: ['Stephen Curry', 'Klay Thompson', 'Draymong Green', 'Andrew Bogut', 'Gestus Ezeli'],
+   },
+
+   {year: '2013-2014',
+   team: 'San Antonio Spurs',
+   logo: 'https://images-na.ssl-images-amazon.com/images/I/51Zr5gUw52L._SY300_.jpg',
+   players: ['Tim Duncan', 'Tony Parker', 'Manu Ginobili', 'Kawhi Leonard', 'Tiago Splitter'] ,
+  },
+  ]
+}
