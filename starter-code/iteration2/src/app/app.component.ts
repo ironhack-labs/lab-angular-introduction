d83b24c67bfc4a938d65f2f22370b04a2a34f5d4
Josep Salvat <jsalvat87@gmail.com>
2018-02-19 20:32:03 +0100

    ara si, hasta iteration2

---
+++ starter-code/iteration2/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Learn how to code in Ironhack';
+  image = 'https://picsum.photos/458/354?random';
+  description = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem ';
+}
