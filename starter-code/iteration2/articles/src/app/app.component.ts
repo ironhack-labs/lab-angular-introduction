15bbd44d1086e5960591d46d5f2c303485871348
Ana Recuero <anarecsa@gmail.com>
2018-02-19 18:06:09 +0100

    iteration 2

---
+++ starter-code/iteration2/articles/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'La increíble historia de los gatos flotantes';
+  imgUrl:string = "https://1.bp.blogspot.com/-SWL-YKxiyBA/V9q_Y5aLmiI/AAAAAAAAAAk/2h8-1drqoz0ZKGCpNc-CsfoIJVSWTUctgCLcB/s1600/Tanialoka3.gif";
+  description:string = `Dragée brownie pie. Liquorice biscuit chocolate bar pudding pie. Candy canes gummies gummies oat cake candy canes jujubes sugar plum.
+  Fruitcake chocolate tootsie roll cotton candy. Jelly-o cotton candy fruitcake. Chupa chups lollipop jelly beans. Pudding chocolate bar ice cream sesame snaps dessert ice cream.
+  Bear claw powder lollipop muffin. Tart carrot cake pastry. Dessert brownie soufflé apple pie carrot cake gummi bears chocolate bar marzipan chocolate bar. Toffee toffee macaroon biscuit halvah.`;
+}
