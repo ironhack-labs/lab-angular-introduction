d908e84e1fe5c9fa73ed5f2d91c876c6bba2aa7a
AlvaroMata8 <alvaromataaracil@hotmail.com>
2018-02-19 17:32:19 +0100

    Iteracion 1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Manu.pow(manu)';
+  images:Array<object>=[
+    {
+      title : 'Risas1',
+      description : 'paco',
+      imgUrl : 'http://www.onepiecelovers.net/wp-content/uploads/2017/01/445262-one-piece-roronoa-zoro-660x330.jpg'
+    },
+    {
+      title : 'Risas2',
+      description : 'josue',
+      imgUrl : 'http://cdn.atomix.vg/wp-content/uploads/2016/01/anime-one-punch-man-atomix-04.png'
+    },
+    {
+      title : 'Risas3',
+      description : 'herminia',
+      imgUrl : 'http://3.bp.blogspot.com/-BjbpVQLejOI/UqgpXHopjuI/AAAAAAAABIk/5VDasGBzDPQ/s1600/abuela_rockera.jpg'
+    },
+
+  ]
 }
+
+
