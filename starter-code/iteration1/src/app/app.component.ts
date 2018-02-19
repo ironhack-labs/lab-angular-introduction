99c514da132acfa8795600a0c7ae4baf1dfddfc5
Alfio <alfioanr@gmail.com>
2018-02-19 18:41:07 +0100

    iteraciones completas

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string = "Dabbing unicorn";
+  imgUrl: string = "https://res.cloudinary.com/teepublic/image/private/s--OqMrefCa--/t_Preview/b_rgb:c7c7c7,c_limit,f_jpg,h_630,q_90,w_630/v1494469473/production/designs/1595608_1.jpg";
+  tittle1: string = "homer con Binoculares";
+  imgUrl1: string = "https://cdn.cnn.com/cnnnext/dam/assets/141216183300-simpsons-25-anniversary-image-4-horizontal-large-gallery.jpg";
+  tittle2: string = "Trolling Face";
+  imgUrl2: string = "http://i0.kym-cdn.com/entries/icons/original/000/000/091/TrollFace.jpg"
 }
