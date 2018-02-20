d34fd69eccfaeeb01f7da4366f322b4b444490c3
Jesús Silva <hola@jesussilva.es>
2018-02-20 09:13:56 +0100

    Tercera iteracion ok

--- starter-code/iteration3/src/app/app.component.ts
+++ starter-code/iteration3/src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  champions = [
-    { year: '2015-2016', team: 'Cleveland Cavaliers', img: '../../assets/img/cc.png', players: ['Lebron James', 'Kyrie Irving', 'Kevin Love', 'J.R. Smith', 'Andreson Varejao'] },
-    { year: '2016-2017', team: 'Golden State Warriors', img: '../../assets/img/gsw.png', players: ['Stephen Curry', 'Klat Thompson', 'Draymong Green', 'Andrew Bogut', 'Gestus Ezeli'] },
-    { year: '2013-2014', team: 'San Antonio Spurs', img: '../../assets/img/sas.png', players: ['Tim Duncan', 'Tony Parker', 'Manu Ginóbili', 'Kawhi Leonard', 'Tiago Splitter'] },
-  ]
+  title = 'Champions';
 }
--- starter-code/iteration3/src/app/champions/champions.component.ts
+++ starter-code/iteration3/src/app/champions/champions.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./champions.component.css']
 })
 export class ChampionsComponent implements OnInit {
-
+  title = 'Check it out!';
+  champions = [
+    { year: '2015-2016', team: 'Cleveland Cavaliers', img: '../../assets/img/cleveland.png', players: ['Lebron James', 'Kyrie Irving', 'Kevin Love', 'J.R. Smith', 'Andreson Varejao'] },
+    { year: '2016-2017', team: 'Golden State Warriors', img: '../../assets/img/golden.png', players: ['Stephen Curry', 'Klat Thompson', 'Draymong Green', 'Andrew Bogut', 'Gestus Ezeli'] },
+    { year: '2013-2014', team: 'San Antonio Spurs', img: '../../assets/img/san_antonio.png', players: ['Tim Duncan', 'Tony Parker', 'Manu Ginóbili', 'Kawhi Leonard', 'Tiago Splitter'] },
+  ]
   constructor() { }
 
   ngOnInit() {
