5e8b1264ffd9bd8a1acdc792c08cd0a5fc4384c6
nachobaile <nachobaile@gmail.com>
2018-02-20 09:34:18 +0100

    iteration2 finish

---
+++ starter-code/iteration2/src/app/main-component/main-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-component',
+  templateUrl: './main-component.component.html',
+  styleUrls: ['./main-component.component.css']
+})
+export class MainComponentComponent implements OnInit {
+  title="pajarito bombo inferno"
+  img="http://3.bp.blogspot.com/-4UTS2h4eHdE/T3sWMHq9OqI/AAAAAAAAAHA/RzJgxi2ubgY/s1600/filosofos+presocr%C3%A1ticos+ok.png"
+  description= "Loretus ijkhqawfjovdnlkvfasdmnlkasvfnmlkasvfdnkll,  okejfksafokfjaslkfñl  pasfkjpaksfjfkpsjwkpd soy ptograsmador jowqndijasjDJKSNFDJLNASWolknaslkmflkñsmafkpmSDL,KMDSALKÑmdasLKÑSMDSAKMNFASLK NFASLKNCDLSK<<mklmaXMLK ĆDASKOLPFASKDOPPOAEFJVPFOIASM   HYYBT T GTG T4 G4 GG RGR "
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
