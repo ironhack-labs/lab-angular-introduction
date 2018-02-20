e7c203081550b3d0250ae8cfb9036a2964dfd3a6
Filipe Rainho <filipenrainho@gmail.com>
2018-02-20 08:55:14 +0100

    Finished iteration 3

--- starter-code/iteration3/src/app/components/champions/champions.component.ts
+++ starter-code/iteration3/src/app/components/champions/champions.component.ts
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./champions.component.css']
 })
 export class ChampionsComponent implements OnInit {
+  champions = [
+    {
+      year: '2015-16',
+      name: 'Clevland Cavaliers',
+      championUrl:
+        'https://i.cdn.turner.com/nba/nba/.element/media/2.0/teamsites/cavaliers/images/170531-global-logo.png',
+      bestPlayers: [
+        'Lebron James',
+        'Kevin Irving',
+        'Kevin Love',
+        'J. R. Smith',
+        'Anderson Varejao'
+      ]
+    },
+    {
+      year: '2015-16',
+      name: 'Clevland Cavaliers',
+      championUrl:
+        'https://i.cdn.turner.com/nba/nba/.element/media/2.0/teamsites/cavaliers/images/170531-global-logo.png',
+      bestPlayers: [
+        'Lebron James',
+        'Kevin Irving',
+        'Kevin Love',
+        'J. R. Smith',
+        'Anderson Varejao'
+      ]
+    },
+    {
+      year: '2015-16',
+      name: 'Clevland Cavaliers',
+      championUrl:
+        'https://i.cdn.turner.com/nba/nba/.element/media/2.0/teamsites/cavaliers/images/170531-global-logo.png',
+      bestPlayers: [
+        'Lebron James',
+        'Kevin Irving',
+        'Kevin Love',
+        'J. R. Smith',
+        'Anderson Varejao'
+      ]
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
