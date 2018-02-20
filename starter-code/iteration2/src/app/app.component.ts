86c710f98074187550e69992fd8e2bb9a823b1f2
lindaluisa <lindaluisarosenthal@gmail.com>
2018-02-20 09:12:03 +0100

    iteration 1

---
+++ starter-code/iteration2/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { Hero } from '../hero';
+
+// @Component({
+//   selector: 'app-heroes',
+//   templateUrl: './heroes.component.html',
+//   styleUrls: ['./heroes.component.css']
+// })
+// export class HeroesComponent implements OnInit {
+//   hero: Hero = {
+//     id: 1,
+//     name: 'Windstorm'
+//   };
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
