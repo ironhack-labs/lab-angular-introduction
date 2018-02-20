037407ee9988deb9f5e8a0862682a375e28ea7fc
theRealSheng <sheng.huchai@gmail.com>
2018-02-20 08:37:45 +0100

    first iteration

---
+++ starter-code/iteration2/starter-code/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
