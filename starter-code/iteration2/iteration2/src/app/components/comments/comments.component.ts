2c1f872431ff6f305852f05743efc3325616d9d8
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-02-20 09:53:16 +0100

    fixed iteration 2

--- starter-code/iteration2/iteration2/src/app/components/comments/comments.component.ts
+++ starter-code/iteration2/iteration2/src/app/components/comments/comments.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent implements OnInit {
-
+comments: Array<{}> = [
+  {
+    name: 'Danielle',
+    comment: 'de puta madre'
+  },
+  {
+    name: 'Shane',
+    comment: 'applause'
+  }
+]
   constructor() { }
 
   ngOnInit() {
