55e83ef76b19cd010b0741bf8cd4b27c1aab9814
diana-91 <diana.hervasmoya@gmail.com>
2018-03-03 12:43:10 +0100

    iteration1 template

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',  
+  template: `
+    <style>
+      img{ width: 400px; height: 300px;}
+      p{font: 22px; text-align: center;}
+      .imagenIronhack{ float:left; margin: 5px;}
+    </style>
+    <div class="imagenIronhack">
+      <img src="https://www.ironhack.com/0be4adcffad57cfca79dbf64b3889fc2.jpg">
+      <p> Ironhack Madrid </p>
+    </div>
+
+    <div class="imagenIronhack">
+      <img src="http://blog.ironhack.com/wp-content/uploads/2016/07/hectorges-54-1024x683.jpg">
+      <p> Ironhack Barcelona </p>
+    </div>
+
+    <div class="imagenIronhack">
+      <img src="https://cdn.evbuc.com/eventlogos/130268051/img3301128129.jpg">
+      <p> Ironhack Miami </p>
+    </div>
+  `
 })
 export class AppComponent {
   title = 'app works!';
