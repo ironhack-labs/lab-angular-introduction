8333a75b369afd5ebcc2ef74826ffedc28e28165
diana-91 <diana.hervasmoya@gmail.com>
2018-03-03 16:08:25 +0100

    iteration2 sin terminar

---
+++ starter-code/iteration2/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    render main
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
