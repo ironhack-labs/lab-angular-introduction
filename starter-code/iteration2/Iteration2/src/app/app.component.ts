c0dfa3b481e2b4f556cda04c4a575a6d671ece81
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-03-03 15:53:24 +0100

    first commit

---
+++ starter-code/iteration2/Iteration2/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <app-main-component></app-main-component>
+  <app-coments-components></app-coments-components>
+  `,
+  
+})
+export class AppComponent {
+  title = 'app';
+}
---
+++ starter-code/iteration2/Iteration2/src/app/coments-components/coments-components.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coments-components',
+  templateUrl: './coments-components.component.html',
+  styleUrls: ['./coments-components.component.css']
+})
+export class ComentsComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
