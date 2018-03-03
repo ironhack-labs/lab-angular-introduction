3a5c7c2ee9a09f4dfd6a8c10215778c4a189bb3b
Antonella Briatore <antobria@gmail.com>
2018-03-03 16:03:19 +0100

    iteration3:half

--- starter-code/iteration2/blog/src/app/app.module.ts
+++ starter-code/iteration2/blog/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
-import { AlbumComponent } from './album/album.component';
+import { CommentsComponentComponent } from './comments-component/comments-component.component';
+import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlbumComponent
+    CommentsComponentComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
