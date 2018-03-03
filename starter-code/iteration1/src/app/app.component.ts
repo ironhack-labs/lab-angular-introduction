dca8aaee2d56687e14e750c9b9ae1a292b0d50e6
Barbara Sanser <bsanser@gmail.com>
2018-03-03 12:45:58 +0100

    feat: iteration 1 completed

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  pictures: Array<{imgSrc : string, footer : string}> =  [
+   {imgSrc:'https://www.ironhack.com/0be4adcffad57cfca79dbf64b3889fc2.jpg', footer:'Ironhack Madrid'},
+   {imgSrc:'http://blog.ironhack.com/wp-content/uploads/2017/01/best-coding-botcamp-ironhack-student-podcast-blog-header.jpg', footer:'Ironhack Barcelona'},
+  {imgSrc:'https://dgivdslhqe3qo.cloudfront.net/careers/photos/20758/normal_photo_1479913482.jpg',footer:'Ironhack Miami'}
+
+  ];
+
+  photo: 'https://dgivdslhqe3qo.cloudfront.net/careers/photos/20758/normal_photo_1479913482.jpg';
 }
