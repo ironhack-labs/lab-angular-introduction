81931014caf5cdb844f7b43c0b3001819aec0d98
Alvaro Pompa <alvaropompa@gmail.com>
2018-03-03 15:05:11 +0100

    feat: iteration 3 complete

--- starter-code/iteration3/Champion/src/app/app.component.ts
+++ starter-code/iteration3/Champion/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  Championships: Array < object > = [{
+      first: '2015-16',
+      name: 'Cleveland Cavaliers',
+      img: 'https://piratasdelbasket.net/wp-content/uploads/2017/05/cleveland-cavaliers.jpg',
+      best: ['Lebron James', 'asdasd', 'qweqwe', 'asdasd', 'gggffg']
+    },
+    {
+      first: '2014-15',
+      name: 'Cleveland Cavaliers',
+      img: 'http://i.cdn.turner.com/nba/nba/.element/img/1.0/teamsites/logos/teamlogos_500x500/gsw.png',
+      best: ['Stephen Curry', 'aaaaa', 'bbbbb', 'cccc', 'dddd']
+    },
+    {
+      first: '2013-14',
+      name: 'Cleveland Cavaliers',
+      img: 'http://i.cdn.turner.com/nba/nba/.element/img/1.0/teamsites/logos/teamlogos_500x500/sas.png',
+      best: ['Tim Dncan', 'qqqq', 'wwwww', 'dddd', 'rrrrr']
+    }
+  ];
 }
