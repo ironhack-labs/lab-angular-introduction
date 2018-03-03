4b9c2ade162685fe52c5ac78d6fb3cc15b31dbe9
LAURA MARCOS AGUILAR <Laura@MacBook-Pro-de-LAURA.local>
2018-03-03 13:30:41 +0100

    iteration 2: done

---
+++ starter-code/iteration1/src/app/argicle01/argicle01.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-argicle01',
+  templateUrl: './argicle01.component.html',
+  styleUrls: ['./argicle01.component.css']
+})
+export class Argicle01Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- starter-code/iteration4/src/app/app.module.ts
+++ starter-code/iteration4/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
-import { Argicle01Component } from './argicle01/argicle01.component';
+import { MainComponentComponent } from './main-component/main-component.component';
+import { CommentsComponentComponent } from './comments-component/comments-component.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Argicle01Component
+    MainComponentComponent,
+    CommentsComponentComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
