0220906de907c5a94a4b1832d1c344ef4eb5ba0b
LAURA MARCOS AGUILAR <Laura@MacBook-Pro-de-LAURA.local>
2018-03-03 12:39:29 +0100

    iteracion 1: done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Disney Parks!';
+  description01:string = 'Disneyland Paris';
+  description02: string = 'Disneyland California';
+  description03: string = "Disneyland Tokyo";
+  img01: string = "../assets/img/paris.jpg";
+  img02: string = "../assets/img/california.jpeg";
+  img03: string = "../assets/img/tokio.jpg";
 }
