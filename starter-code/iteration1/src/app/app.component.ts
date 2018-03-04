a560ee9eff5003e1e0af306fe6434009427b0852
Cristina Bertrand <cristina@11870.com>
2018-03-04 19:21:48 +0100

    feat:iteration1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Iteration1';
+
+  offices: Array<{picture: string, location: string}> = [
+    {picture:"https://www.ironhack.com/0be4adcffad57cfca79dbf64b3889fc2.jpg", location:"Ironhack Madrid"},
+    {picture:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdNbZxyikEnDBOhvwViNw4sLSRZerH808aDVjbH6PjzvxXlLg9", location:"Ironhack Barcelona"},
+    {picture:"http://blog.ironhack.com/wp-content/uploads/2017/01/best-coding-botcamp-ironhack-student-podcast-blog-header.jpg", location:"Ironhack Miami"}
+  ]
 }
