9c9faf945dce477654d9dc1a8781251d7d9f3aa0
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-03-05 13:20:18 +0100

    iteration2

--- starter-code/iteration2/Iteration2/src/app/coments-components/coments-components.component.ts
+++ starter-code/iteration2/Iteration2/src/app/coments-components/coments-components.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coments-components.component.css']
 })
 export class ComentsComponentsComponent implements OnInit {
-
-  constructor() { }
+comment:string;
+constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
