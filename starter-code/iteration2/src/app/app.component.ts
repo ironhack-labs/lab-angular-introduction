559ea98504c14907e9b31080e06f707fafe5c69b
Francis Calizo <francisacalizo@gmail.com>
2018-03-05 18:19:16 -0500

    complete

--- starter-code/iteration2/src/app/app.component.ts
+++ starter-code/iteration2/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Learn How to Eat with Ironhack';
+
+  image = '../assets/images/gordo.jpg';
+
+  description = `
+  Bacon ipsum dolor amet tail swine strip steak shoulder cow pancetta. Tail landjaeger shank meatball strip steak t-bone turducken. 
+  Leberkas tongue capicola tenderloin kielbasa. Fatback boudin tri-tip landjaeger biltong chicken andouille pork loin, pork belly ham. 
+  Ground round spare ribs fatback andouille ribeye shank pig boudin. Pig spare ribs picanha rump t-bone andouille tail chicken ball tip 
+  strip steak boudin beef turducken kielbasa landjaeger. Porchetta boudin short ribs tongue ball tip pork.
+  `;
+
+  
 }
