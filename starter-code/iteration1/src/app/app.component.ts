081c3ecb1f3276ef4335c1fd797c4d766455a6f5
DiegoArroyo <arroyodiego1@gmail.com>
2018-03-07 09:31:36 -0600

    Iteration One

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Iteration One';
+
+  album: Array<Object> = [
+    {
+      name: 'Rick',
+      photo: 'https://http2.mlstatic.com/vinilo-decorativo-rick-n-morty-rick-sanchez-D_NQ_NP_833675-MLM26291459217_112017-F.jpg',
+      description: 'This is pic 1'
+    },
+    {
+      name: 'Morty',
+      photo: 'https://i.imgur.com/BiA02pY.png?1',
+      description: 'This is pic 2'
+    },
+    {
+      name: 'Rick & Morty',
+      photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScL7CmfmNqZVzlrBvrTssPCFguLyBDC_sIjcTACtwmMkyzz26U',
+      description: 'This is pic 2'
+    }
+  ];
 }
+
