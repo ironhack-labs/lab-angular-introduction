5b9b6bcf5bdf239718a26c0ac23c363209c7ac0f
Borja Arana <barana1977@hotmail.com>
2018-04-30 19:04:35 +0200

    Final ejercicio

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  img1 = "http://www.sincortapisa.com/wp-content/uploads/2018/03/promo328637357.jpg";
+  img2 = "https://s2.eestatic.com/2018/04/27/deportes/motor/f1/Fernando_Alonso-Carlos_Sainz_Jr-Azerbaiyan-Mundial_de_Formula_1-F1-F1_302983339_75539335_3500x2334.jpg";
+  img3 = "http://images.performgroup.com/di/library/sportal_com_au/49/1d/daniel-ricciardo_m6bhh70qkkur1xlgvwoloatp0.jpg?w=1280&h=720&quality=100"
+
 }
