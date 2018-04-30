eb4d1c5cf699d7216a5dedbcb86c53fb01ebada1
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-30 19:18:55 +0200

    Iteration 2 completed

---
+++ starter-code/iteration2/angular-article/src/app/main-component/main-component.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-component',
+  templateUrl: './main-component.component.html',
+  styleUrls: ['./main-component.component.css']
+})
+export class MainComponentComponent {
+
+  constructor() { }
+
+  title = 'Learn how to code with Ironhack';
+  image = 'https://images.adsttc.com/media/images/591a/5af2/e58e/ced8/ef00/009d/slideshow/6.jpg?1494899430';
+  description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at commodo elit. Nullam faucibus quam a est euismod, a pulvinar mi fringilla. Vestibulum vitae tortor vel elit venenatis tempor. Quisque viverra luctus leo, eget porttitor velit aliquet ac. Cras pellentesque, mi a hendrerit suscipit, eros nisl mattis ex, at bibendum nisi lectus in dui. Aenean iaculis enim cursus dignissim elementum. Duis nec tempus quam. Integer luctus magna fringilla augue mattis lacinia. Quisque et neque congue, porta justo sed, varius tortor. Nulla ac dictum lacus. Donec velit neque, vestibulum sit amet ultrices ac, feugiat tristique mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla erat tellus, lobortis ac felis in, sagittis consectetur orci. Quisque orci felis, aliquet at metus sed, congue mollis justo.';
+
+}
