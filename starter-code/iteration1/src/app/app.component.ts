f82d0ef12a32ec071f5ce01dd77fb8e8555b9a7f
A-Tr <alvarotrancon@gmail.com>
2018-04-30 16:26:32 +0200

    iteracion 1 done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "app works!";
+  img = [
+    {name: "Iron Man", src: "https://vignette.wikia.nocookie.net/marvel/images/8/8d/44-_Armadura_Bleeding_Edge.jpg/revision/latest?cb=20130713005209&path-prefix=es"},
+    {name: "Thor", src: "https://img.bekia.es/articulos/portada/62000/62107-h2.jpg"},
+    {name: "Hulk", src: "http://contenidos.enter.co/custom/uploads/2017/10/maxresdefault-1-1.jpg"}
+  ];
 }
