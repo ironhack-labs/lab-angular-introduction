05e89a24c78494664d98d88c550a3a92b2aeb2c3
Germán Rodríguez <germanrodriguez@MacBook-Pro-de-German.local>
2018-04-30 20:37:30 +0200

    Hello Byron, Fillipe, Eloi. You are my favorite TAS

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "app works!";
+  photos: Array<Object> = [
+    {
+      path: "http://nightdeveloper.net/wp-content/uploads/2014/12/mongo_db.png",
+      title: "MongoDB"
+    },
+    {
+      path: "http://www.timlisiecki.com/img/skills/express.png",
+      title: "Express"
+    },
+    {
+      path:
+        "http://blog.hostdime.com.co/wp-content/uploads/angularjs-como-programar.png",
+      title: "Angular"
+    },
+    {
+      path: "https://victorroblesweb.es/wp-content/uploads/2016/03/nodejs.png",
+      title: "Node"
+    }
+  ];
 }
