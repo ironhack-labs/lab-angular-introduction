8e7ef6a6a5b79085768ba3bc3929e8f69c4886ae
Pablo García <heypablete@gmail.com>
2018-04-30 18:18:10 +0200

    iteration 1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  descripMad = "hola Madrid";
+  descripMia = "hola Miami";
+  descripBcn = "hola Barcelona";
+  imgMad = "https://www.arquitecturayempresa.es/sites/default/files/content/arquitecturayempresa_ironhack_02.jpg";
+  imgMia = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOQk10qHD1B696X9Irarx00bn66JF-JXLNGaLvADTE7aQEenTA";
+  imgBcn = "https://www.kippel01.com/files//fotos/ironhack/ironhack-equipo-948.jpg";
+}
+
+export class album {
+
 }
