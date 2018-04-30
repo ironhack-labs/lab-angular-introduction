c18e393efec5026d8b5a236633d8c7dddea36e34
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2018-04-30 19:14:45 +0200

    first iteration done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  photos: Array<Object> = [
+    { path: "http://nightdeveloper.net/wp-content/uploads/2014/12/mongo_db.png", title:"MongoDB"},
+    { path: "http://www.timlisiecki.com/img/skills/express.png", title:"Express"},
+    { path: "http://blog.hostdime.com.co/wp-content/uploads/angularjs-como-programar.png", title: "Angular" },
+    { path: "https://victorroblesweb.es/wp-content/uploads/2016/03/nodejs.png", title: "Node" }
+]
 }
