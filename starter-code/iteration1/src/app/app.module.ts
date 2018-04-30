91273734a8f648041343501d76ec8f684763217d
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-30 18:18:51 +0200

    Iteration 1 completed

--- starter-code/iteration1/src/app/app.module.ts
+++ starter-code/iteration1/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component
   ],
   imports: [
     BrowserModule,
