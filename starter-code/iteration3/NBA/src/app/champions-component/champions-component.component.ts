bea0e9cf2dec7dea0c963043615b12002ea3cd58
Raul.dll <rdl.llave@gmail.com>
2018-04-30 20:53:21 +0200

    DONE

---
+++ starter-code/iteration3/NBA/src/app/champions-component/champions-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-champions-component',
+  templateUrl: './champions-component.component.html',
+  styleUrls: ['./champions-component.component.css']
+})
+export class ChampionsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
