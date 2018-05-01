cb42fadc3b4fe7b85b435afe35bb4aa0566ec56d
lenniez <lenniezhu@gmail.com>
2018-05-01 09:52:30 +0200

    updated with working css

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  private photo1 = require("../assets/1.jpg");
+  private photo2 = require("../assets/2.jpg");
+  private photo3 = require("../assets/3.jpg");
 }
