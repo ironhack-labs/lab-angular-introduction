b41e5e02907cc4b7cd09a5df405bb392c0a17eac
Elise Griset <elise.griset@gmail.com>
2018-05-01 14:32:08 +0200

    done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app works!';
+  // title = 'app works!';
+  photos: Array<Photo> = [
+    new Photo(
+      'Jaleous',
+      'https://media.giphy.com/media/lL2fM6RWgf9ReUJLwV/giphy.gif'
+    ),
+    new Photo(
+      "Kate's hairs",
+      'https://media.giphy.com/media/EKvQpJrlt9lTO/giphy.gif'
+    ),
+    new Photo(
+      'Idiot Bachelor',
+      'https://media.giphy.com/media/3dizFGQ3gAdd29AlkR/giphy.gif'
+    ),
+  ];
+  constructor() {}
+}
+class Photo {
+  constructor(public name: string, public img: string) {}
 }
---
+++ starter-code/iteration2/iteration2/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'What a beautiful article';
+  image = 'https://media.giphy.com/media/3o7abldj0b3rxrZUxW/giphy.gif';
+  description = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt velit corporis corrupti aperiam excepturi quaerat fugiat deleniti, exercitationem repellat eum provident. Nihil magni obcaecati amet assumenda nam, accusamus cum iusto!';
+}
