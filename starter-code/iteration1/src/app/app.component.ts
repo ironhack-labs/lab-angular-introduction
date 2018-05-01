0747d3f376a9568a4a402b2db716ed9345a32bc9
muflonex <muflonex@gmail.com>
2018-05-01 10:25:09 +0200

    One push moar

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Photos';
+  photos: Array<Object> = [
+    {
+      id: 1,
+      title: 'Homer',
+      url: '../assets/homer.png'
+    }, {
+      id: 2,
+      title: 'Marge',
+      url: '../assets/marge.png'
+    }, {
+      id: 3,
+      title: 'Bart',
+      url: '../assets/bart.png'
+    }
+  ];
 }
