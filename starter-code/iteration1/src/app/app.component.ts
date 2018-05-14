e75f2a6cc7d819f883617266e5d34c176428062b
Dali Rojas <rojasdali@gmail.com>
2018-05-14 17:19:57 -0400

    done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  imgUrl = ['https://pbs.twimg.com/profile_images/804432703189843969/fZjDWPmK_400x400.jpg', 'https://www.conciti.org/wp-content/uploads/ironhack_logo.jpg', 'https://static1.squarespace.com/static/56e2e0c520c6472a2586add2/t/594d23b7e6f2e1d4a11b69a3/1498231447084/?format=500w']
+
 }
