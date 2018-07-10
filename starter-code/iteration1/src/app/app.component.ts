08add8cb6df899987c653e10fb74f20a0cf79cda
Meri-Pier Ligioso <mpligioso@gmail.com>
2018-07-10 14:18:27 +0200

    not done, did the first iteration and was stuck on the 2nd. planning to come back to the exercise

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Ironhack Locations!';
+
+  album: Array<Photo> = [
+    { photoUrl: "https://secure.meetupstatic.com/photos/event/4/f/d/e/600_468140446.jpeg",
+      description: "Ironhack Miami" },
+    { photoUrl: "https://cdn-images-1.medium.com/max/1137/1*vTB9s3-vabseGYBucM1aBA.png",
+      description: "Ironhack Madrid" },
+    { photoUrl: "https://www.iamexpat.nl/sites/default/files/styles/article--full/public/ironhack-tech-school-openhouse-expats.jpg?itok=PmbZmWRN",
+      description: "Ironhack Barcelona" },
+
+  ]
+
+}
+
+class Photo {
+  photoUrl: string;
+  description: string;
 }
---
+++ starter-code/iteration2/Iteration2/src/app/main/main.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+class Article {
+  title: string;
+  image: string;
+  description: string;
+}
