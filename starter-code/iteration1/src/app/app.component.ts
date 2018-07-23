7f958f0cc8b91bccb36e2bf515e92f73c2367ea1
Paula <paulagarizcun@gmail.com>
2018-07-23 18:14:14 +0200

    Iteracion 1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Los animalicos';
+  photos: Array<Object> = [
+    {
+      id: 1,
+      photoUrl: 'http://mediad.publicbroadcasting.net/p/kera/files/201601/shutterstock_137913404.jpg',
+      name: 'Dog'
+    },
+    {
+      id: 2,
+      photoUrl: 'https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr03/2013/3/2/23/enhanced-buzz-orig-1716-1362283530-8.jpg?downsize=715:*&output-format=auto&output-quality=auto',
+      name: 'Duck'
+    },
+    {
+      id: 3,
+      photoUrl: 'https://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg',
+      name: 'Cat'
+    }
+  ];
 }
