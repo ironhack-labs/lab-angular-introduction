51ad2fa7513b4328fad03cb350de8bfdb9b679a8
Sarai Vasquez <svmarinez@hotmail.com>
2018-07-23 21:47:23 +0200

    Iteration 3 DONE

--- starter-code/iteration3/src/app/app.component.ts
+++ starter-code/iteration3/src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  championships= [{
+    year: '2017-18',
+    champion: 'Golden State Warriors',
+    image: '../assets/images/200px-Golden_State_Warriors_logo.svg.png',
+    bestPlayers: [ 'Stephen Curry', 'Kevin Durant', 'Klay Thompson', 'J.R. Smith', 'Draymond Green' ]
+  }, {
+    year: '2016-17',
+    champion: 'Golden State Warriors',
+    image: '../assets/images/200px-Golden_State_Warriors_logo.svg.png',
+    bestPlayers: [ 'Stephen Curry', 'Kevin Durant', 'Klay Thompson', 'J.R. Smith', 'Draymond Green' ]
+  }, {
+    year: '2015-16',
+    champion: 'Cleveland Cavaliers',
+    image: '../assets/images/100px-Cleveland_Cavaliers_logo.svg.png',
+    bestPlayers: [ 'Lebron James', 'Kyrie Irving', 'Kevin Love', 'J. R. Smith', 'Anderson Varejao' ]
+  }];
 }
--- starter-code/iteration3/src/app/app.module.ts
+++ starter-code/iteration3/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
