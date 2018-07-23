5f3d6dc7f2c2cfbbaaffd0ce0b5f3fb846ffd76e
Abel Alonso <abel.alonso@gmail.com>
2018-07-23 18:52:46 +0200

    finished

--- starter-code/iteration2/src/app/app.module.ts
+++ starter-code/iteration2/src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
-
+import { CommentElementComponent } from './comment-element/comment-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CommentElementComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
