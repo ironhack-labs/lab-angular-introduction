b41291dfa982b459bdcdcfac21d29c307ddf427c
Jorge L <kolmax@gmail.com>
2018-07-23 18:08:00 +0200

    Finish daily exercise

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  gallery = [
+    {
+      title: 'Ironhack Madrid',
+      image: 'http://e01-expansion.uecdn.es/assets/multimedia/imagenes/2017/06/20/14979611169142.jpg'
+    },
+    {
+      title: 'Ironhack Barcelona',
+      image: 'https://secure.meetupstatic.com/photos/event/9/f/8/f/600_467620847.jpeg'
+    },
+    {
+      title: 'Ironhack Miami',
+      image: 'http://miamiherald.typepad.com/.a/6a00d83451b26169e201bb08a0c81c970d-pi'
+    }
+  ]
 }
---
+++ starter-code/iteration2/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Learn how to code with Ironhack';
+  image = 'http://gratoparquet.com/wp-content/uploads/Coleccion-GRATO-The-Essentials-img.jpg';
+  description = "Just say anything, George, say what ever's natural, the first thing that comes to your mind. Take that you mutated son-of-a-bitch. My pine, why you. You space bastard, you killed a pine. You do? Yeah, it's 8:00. Hey, McFly, I thought I told you never to come in here. Well it's gonna cost you. How much money you got on you?";
+}
