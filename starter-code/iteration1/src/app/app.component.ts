24e4a487ec29e25ac4cade3706ea1c217eb03888
Anna Hogberg <am.hogberg@gmail.com>
2018-07-23 17:03:13 +0200

    iteracion 1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Iteración 1';
+
+ ironhacks: Array<Photo> = [
+    {location:"Ironhack Madrid", pictureUrl:"https://www.arquitecturayempresa.es/sites/default/files/content/arquitecturayempresa_ironhack_02.jpg"},
+    {location:"Ironhack Barcelona", pictureUrl:"https://www.dreamstudiesabroad.com/images/schools/ironhack/6i3qkjzfbl.jpg"},
+    {location:"Ironhack Miami", pictureUrl:"http://blog.ironhack.com/wp-content/uploads/2016/06/building2.jpg"},
+  ];
 }
+   
+class Photo {
+  location: string;
+  pictureUrl: string;
+  }
+
