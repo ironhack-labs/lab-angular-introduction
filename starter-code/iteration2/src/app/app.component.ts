6c94b07f37b1091d01f7eb9ba31c5a6ff4c73331
Sandra Najera <snajera@ucm.es>
2018-07-23 22:42:32 +0200

    Done

--- starter-code/iteration2/src/app/app.component.ts
+++ starter-code/iteration2/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '¿Por qué me muero de sueño?';
+  content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu ex non nisi tempus egestas. Ut fermentum vitae libero quis dapibus. Praesent rutrum sed eros quis rhoncus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam a lobortis massa. Duis ac dolor at diam pretium varius et id quam. Suspendisse euismod dictum libero ut feugiat. Duis et turpis eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.'
 }
--- starter-code/iteration3/src/app/app.module.ts
+++ starter-code/iteration3/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyFirstComponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
