a877fc3ef5133d076fe6314abeccae07c7118709
Anna Hogberg <am.hogberg@gmail.com>
2018-07-23 18:23:55 +0200

    iteracion 2

---
+++ starter-code/iteration2/src/app/comments/comments.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+const moment = require('moment');
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.css']
+})
+export class CommentsComponent {
+title ="Comments"
+  comments: Array<Comment> = [
+    {name: "Anna", text:"Love the city!!", day: new Date()},
+    {name: "Madeleine", text:"My favorite city", day: new Date()},
+  ]
+
+}
+class Comment {
+  name: string;
+  text: string;
+  day: Date ;
+}
---
+++ starter-code/iteration2/src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent  {
+  city = "Madrid"
+  title = "Learn how to Code with Ironhack"
+  img ="https://www.esmadrid.com/sites/default/files/styles/content_type_full/public/editorial/Madrid_undia_1397223554.735.jpg?itok=4ziKw0gD"
+  description ="Madrid (/məˈdrɪd/, Spanish: [maˈðɾið]) is the capital of Spain and the largest municipality in both the Community of Madrid and Spain as a whole. The city has almost 3.2 million[4] inhabitants and a metropolitan area population of approximately 6.5 million. It is the third-largest city in the European Union (EU), smaller than only London and Berlin, and its monocentric metropolitan area is the third-largest in the EU, smaller only than those of London and Paris. The municipality covers 604.3 km2 (233.3 sq mi)."
+  }
+
+
