9465e7a63fd31eb15f7123c5258c7a149e6cc5c4
JuanGPC <juangpc@gmail.com>
2018-07-23 17:50:29 +0200

    iter1 completed

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Daily Project!';
+  tagline = "You'll find nothing better to do."
+  photos:Array<Object>=[
+    {
+      img:"http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/ep_Astrocyte5_16x9.jpg?itok=GMIFyBxb",
+      title:"Astrocyte Cell"
+    },
+    {
+      img:"https://upload.wikimedia.org/wikipedia/commons/6/6d/GolgiStainedPyramidalCell.jpg",
+      title:"Hyppocampus Pyramial Cell"
+    },
+    {
+      img:"https://www.thoughtco.com/thmb/MGtRxJTDPwLpvknm7GRoTG7JKyg=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/Neuron-5728bfaa5f9b589e34aa64b0.jpg",
+      title:"Glial cell"
+    }
+  ]
 }
