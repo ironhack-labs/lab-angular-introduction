535d84c19e1282cb1d1d84abd3e64f789074d42e
Ali Sattar <alirsattar@gmail.com>
2018-07-24 10:54:20 -0400

    Form input fields completed

--- starter-code/iteration2/iteration2/src/app/app.module.ts
+++ starter-code/iteration2/iteration2/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { MainComponentComponent } from './main-component/main-component.component';
 import { CommentsComponentComponent } from './comments-component/comments-component.component';
@@ -14,7 +16,8 @@ import { ChampionsComponent } from './champions/champions.component';
     ChampionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- starter-code/iteration2/iteration2/src/app/comments-component/comments-component.component.ts
+++ starter-code/iteration2/iteration2/src/app/comments-component/comments-component.component.ts
@@ -7,18 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsComponentComponent implements OnInit {
 
-  comments:Array<Object> = [{
+  username:string = 'blah'
+
+  comments:Array<any> = [
+    {
     author:     'Sandra',
     content:    'Can someone please teach me .populate ??',
     date:       Date
-  },
-  {
+    },
+    {
     author:     'Ali',
     content:    'Sure. $50 for my seminar.',
     date:       Date
-  }
-  ];
+    }
+    ];
 
+    addComment(author,comment){
+    
+    this.comments.push({
+
+      author:   author.value,
+      content:  comment.value,
+      date:     Date
+
+    })
+    
+      console.log(author.value,comment.value);
+  }
+
+  
+  
   constructor() { }
 
   ngOnInit() {
