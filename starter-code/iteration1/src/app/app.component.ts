c1789dd2d34d908d17648bef17ed9a0dc164f7d9
Gabriel Garcia <gabogarciam71@gmail.com>
2018-07-24 09:40:53 +0200

    BCN DONE

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Photo Album!';
+  seeds = [
+    {
+      "image": 'https://img.europapress.es/fotoweb/fotonoticia_20180309191938_640.jpg',
+      "citie": "Ironhack Madrid"
+    },
+    {
+      "image": 'https://www.kippel01.com/files//fotos/ironhack/ironhack-equipo-948.jpg',
+      "citie": "Ironhack Barcelona"
+    },
+    {
+      "image": 'http://blog.ironhack.com/wp-content/uploads/2016/12/Coding-Bootcamp-Ironhack-Graduates-December-2016-Barcelona-1-920x518.png',
+      "citie": "Ironhack Miami"
+    }
+  ];
 }
---
+++ starter-code/iteration2/src/app/main-component/main-component.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '../../../node_modules/@angular/platform-browser';
+
+@Component({
+  selector: 'app-main-component',
+  templateUrl: './main-component.component.html',
+  styleUrls: ['./main-component.component.css']
+})
+export class MainComponentComponent implements OnInit {
+  title = 'Learn how to code with Ironhack';
+  image = "https://cdn.pixabay.com/photo/2016/12/21/19/20/barcelona-1923686_1280.jpg";
+  description = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
