a3f3c18e0657463e0992c19dd1df793cc624e02b
EdMijangos <mloeza2010@hotmail.com>
2018-07-24 08:47:05 -0500

    DE MEX - Eduardo Mijangos

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works!'
+  imgOne = 'https://78.media.tumblr.com/avatar_666b1add893b_128.pnj';
+  imgTwo = 'https://dogemuchwow.com/wp-content/themes/dogeland/app/dogetizer/images/cool-doge-2-sm.png';
+  imgThree = 'https://78.media.tumblr.com/avatar_d8e48daeecee_128.pnj'
 }
