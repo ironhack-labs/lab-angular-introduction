ab0b8f1e333075e9a3151666857a5519be32299d
Giannini Charles <12gcharles@gmail.com>
2018-07-24 10:51:06 -0400

    Added live update

--- starter-code/iteration2/blah/src/app/comments/comments.component.ts
+++ starter-code/iteration2/blah/src/app/comments/comments.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsComponent {
 title:string = 'Comments'
+
+username:string = 'stock username'
+
 arrayOfComments: Array<any> = [
   
 {
@@ -19,10 +22,17 @@ arrayOfComments: Array<any> = [
   reviewer: 'The Virtuoso',
   review: 'I rehearsed this!'
 }
-
-
-
 ]
+
+
+addComment(author, comment){
+ console.log(author.value, comment.value);
+ this.arrayOfComments.push({
+  reviewer: author.value,
+  review: comment.value
+ });
+//  You have to BIND to an event defined in the same component..
+}
 
 
 }
