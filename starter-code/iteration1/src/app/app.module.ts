b1723abddf4da9aa7ee7d8c02cc9d13fdb633bfb
Felipe Vallina <fvallinalp@gmail.com>
2018-07-25 08:35:42 -0500

    done

--- starter-code/iteration1/src/app/app.module.ts
+++ starter-code/iteration1/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import { MainComponent } from './main/main.component';
 import { AppComponent } from './app.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CommentsComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
