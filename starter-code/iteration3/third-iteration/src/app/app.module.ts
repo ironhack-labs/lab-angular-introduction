84c6703933393ff9fd5ffa1644a4bc37b257448e
Paula Balcells <paulabalcells@gmail.com>
2018-08-07 09:12:53 +0200

    Done

---
+++ starter-code/iteration3/third-iteration/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ChampionsComponent } from './components/champions/champions.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChampionsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
