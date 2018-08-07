31adac1274cb1f6a0798f8fa0ad9c632a012d67a
Marc Pósito <marcposito@gmail.com>
2018-08-07 09:58:02 +0200

    it3 done

--- starter-code/iteration3/iteration3/src/app/app.component.ts
+++ starter-code/iteration3/iteration3/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'iteration3';
-}
+  //Aqui ponemos una variable llamada championship que contiene objetos de equipos, dentro de cada uno un array de strings de mvp. 
+  championships = [{
+      name: 'equipo1',
+      title: 'el gran equipo',
+      players: ['marc','marc','marc','marc','marc']
+    },{
+      name: 'equipo2',
+      title: 'el gran equipo',
+      players: ['marc','marc','marc','marc','marc']
+    },{
+      name: 'equipo3',
+      title: 'el gran equipo',
+      players: ['marc','marc','marc','marc','marc']
+    },{
+      name: 'equipo4',
+      title: 'el gran equipo',
+      players: ['marc','marc','marc','marc','marc']
+    }]
+  
+  };
