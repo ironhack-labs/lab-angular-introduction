22ab5fca566e100e8213b2470b5ea0c13bdfd28f
jhmatienza <jhm.atienza@gmail.com>
2018-09-06 21:24:48 +0200

    new files created and working components iteration1

--- starter-code/iteration1/src/app/app.module.ts
+++ starter-code/iteration1/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AlbumComponent } from './components/album/album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
