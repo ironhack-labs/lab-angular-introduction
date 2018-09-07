d33aaae93956dd4ebb3208b7c76ab698ec1fc399
jhmatienza <jhm.atienza@gmail.com>
2018-09-07 04:45:39 +0200

    first iteration done

--- starter-code/iteration1/src/app/components/album/album.component.ts
+++ starter-code/iteration1/src/app/components/album/album.component.ts
@@ -5,11 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './album.component.html',
   styleUrls: ['./album.component.css']
 })
-export class AlbumComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class AlbumComponent {
 
 }
