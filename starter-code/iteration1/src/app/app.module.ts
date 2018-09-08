c096248589010277e812f15b699cb27002e4b77b
Georgina Pacey <paceygeorgina@gmail.com>
2018-09-08 08:59:03 +0200

    first iteration

--- starter-code/iteration1/src/app/app.module.ts
+++ starter-code/iteration1/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyNestedComponent } from './my-nested/my-nested.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNestedComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
