6aefca2c31d86b8971949ea0c4b8b3c9f70d1051
Alex González <alexgm@MacBook-Pro-de-Alex.local>
2018-09-09 20:21:45 +0200

    Iteration 1 done, iteration 3 wip

--- angular-introduction/src/app/app.module.ts
+++ angular-introduction/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PhotoAlbumComponent } from './components/photo.album/photo.album.component';
 import { PhotoDetailComponent } from './components/photo-detail/photo-detail.component';
+import { NBAChampionsComponent } from './components/nbachampions/nbachampions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PhotoAlbumComponent,
-    PhotoDetailComponent
+    PhotoDetailComponent,
+    NBAChampionsComponent
   ],
   imports: [
     BrowserModule
---
+++ angular-introduction/src/app/components/nbachampions/nbachampions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nbachampions',
+  templateUrl: './nbachampions.component.html',
+  styleUrls: ['./nbachampions.component.css']
+})
+export class NBAChampionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
