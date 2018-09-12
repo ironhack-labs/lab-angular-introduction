99bc1323fb2e38f27e8b9ba9f300d5fe979a88e7
Alex González <alexgm@MacBook-Pro-de-Alex.local>
2018-09-12 17:15:42 +0200

    Iteration 3 done

--- angular-introduction/src/app/components/nbachampions/nbachampions.component.ts
+++ angular-introduction/src/app/components/nbachampions/nbachampions.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Team{
+  year: string,
+  name : string,
+  championUrl:string,
+  bestPlayers: Array<string>
+}
 @Component({
   selector: 'app-nbachampions',
   templateUrl: './nbachampions.component.html',
   styleUrls: ['./nbachampions.component.css']
 })
-export class NBAChampionsComponent implements OnInit {
+export class NBAChampionsComponent{
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  teamChampionships:Array<Team> = [
+    {
+      year: '2015-16',
+      name : 'Cleveland Cavaliers',
+      championUrl:'../../../assets/cavs.png',
+      bestPlayers: ['Lebron James','Kyrie Irving','Kevin Love' , 'Anderson Varejao']
+    },
+    {
+      year: '2014-15',
+      name : 'Golden State Warriors',
+      championUrl:'../../../assets/state.png',
+      bestPlayers: ['Stephen Curry', 'Klay Thompson','Andrew Bogut','Gestus Ezeli']
+    },
+    {
+      year: '2013-14',
+      name : 'San Antonio Spurs',
+      championUrl:'../../../assets/spurs.png',
+      bestPlayers: ['Tim Duncan','Tony Parker','Manu Ginobili','Tiago Splitter']
+    }
+  ]
 }
