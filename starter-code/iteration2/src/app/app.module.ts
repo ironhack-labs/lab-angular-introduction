45b790a1d7b1efb79afef04209710ed61d940907
Daniel Ayuso <daniel.ayuso@innocv.com>
2019-04-02 20:54:44 +0200

    generate main component

--- starter-code/iteration2/src/app/app.module.ts
+++ starter-code/iteration2/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponentComponent } from './main-component/main-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponentComponent
   ],
   imports: [
     BrowserModule,
