7df42f2df78f8a1bcaec05572fb5792b244d9885
Victor Bañuls <victorbanuls@MacBook-Pro-de-Victor.local>
2019-04-02 20:10:01 +0200

    iteration1 done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title:string = 'Ironhack Madrid';
+  image:string = 'https://www.arquitecturayempresa.es/sites/default/files/styles/n1000x540/public/imagenes/noticia/arquitecturayempresa_ironhack_01.jpg?itok=3w9b4gdm';
+  title2:string = 'Ironhack Barcelona';
+  image2:string = 'https://www.arquitecturayempresa.es/sites/default/files/styles/n1000x540/public/imagenes/noticia/arquitecturayempresa_ironhack_01.jpg?itok=3w9b4gdm';
+  title3:string = 'Ironhack Miami';
+  image3:string = 'https://www.arquitecturayempresa.es/sites/default/files/styles/n1000x540/public/imagenes/noticia/arquitecturayempresa_ironhack_01.jpg?itok=3w9b4gdm';
+
 }
