512e76136baef8906da94e07e9faa74d3e026f53
Jesus <jesus.sanz@innocv.com>
2019-04-02 20:09:14 +0200

    iteration1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  img1:string = "http://delpozoaluniverso.com/wp-content/uploads/2017/06/IMG_0979.jpg"
+  img1Title:string = "Ironhack Madrid"
+  img2:string = "http://delpozoaluniverso.com/wp-content/uploads/2017/06/IMG_0979.jpg"
+  img2Title:string = "Ironhack Barcelona"
+  img3:string = "http://delpozoaluniverso.com/wp-content/uploads/2017/06/IMG_0979.jpg"
+  img3Title:string = "Ironhack Miami"
 }
