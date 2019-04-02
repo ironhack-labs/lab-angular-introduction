b5dc6727df86b9e6836f440951f0282fdd201906
j0nl1 <jonlitech@gmail.com>
2019-04-02 20:57:16 +0200

    First Iteration

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  ironhackMadrid = new PhotoCard("http://blog.ironhack.com/wp-content/uploads/2018/11/madrid.jpg", "Ironhack Madrid")
+  ironhackBarcelona = new PhotoCard("https://www.ironhack.com/7cf6338ea059170fd98b01f4db9629a9.jpg", "Ironhack Barcelona")
+  ironhackMiami = new PhotoCard("https://www.ironhack.com/370680c403f3a3d4a29149716547e25f.jpg", "Ironhack Miami")
+}
+
+class PhotoCard {
+  img: string = ''
+  description: string = ''
+  constructor(imgUrl: string, description: string) {
+    this.img = imgUrl
+    this.description = description
+  }
 }
