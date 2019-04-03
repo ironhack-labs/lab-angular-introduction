29976fd1b67c0c071969ae93b9646e0c572fa7dd
Roberto S <roberto.salinas@innocv.com>
2019-04-03 08:44:32 +0200

    1st iteration done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'App';
+  im1='https://i.ytimg.com/vi/yB-3fjCgUKg/hqdefault.jpg';
+  im1Name = 'Macario';
+  im2='https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/03/31/15540255587586.jpg'
+  im2Name = 'Nick';
+  im3='https://ep01.epimg.net/elpais/imagenes/2017/10/13/que_hace_ahora/1507896337_398018_1507899496_sumario_normal.jpg'
+  im3Name = 'Steve';
 }
