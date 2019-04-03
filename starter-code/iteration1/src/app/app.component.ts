d5e3c48e7cf04ccbea61de49d250666964e5f04e
AlvaroCS <alvaro.corral@innocv.com>
2019-04-03 08:08:07 +0200

    It1

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title1 = 'La abstemia';
+  title2 = 'El más grande';
+  title3 = 'Tu madre era así cuando era joven, asúmelo.';
+  // dimg='/assets/masiel/fot.jpg';
+  //<img src='https://nicolasramospintado.files.wordpress.com/2013/12/dile-karina-1963.jpg'>
 }
